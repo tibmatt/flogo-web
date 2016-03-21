7b03e4a690e674899effbd78218d0714b34d6186
Shaoke Xu <shaoxu@tibco.com>
2016-03-20 21:58:40 -0700

    Use angular2-seed as example project, working on create the basic structure of Flogo web app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
+**This is draft version, I am still working on add more**
+
 # flogo-web
 Flogo web include flogo front-end and flogo design time server
-
-
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import path from 'path';
 import runSequence from 'run-sequence';
+import del from "del";
 import gulpLoadPlugins from 'gulp-load-plugins';
 
 //CONFIG
@@ -16,23 +17,82 @@ CONFIG.server=path.join(CONFIG.source, 'server');
 CONFIG.dist= 'dist';
 CONFIG.public=path.join(CONFIG.dist, 'public');
 
+CONFIG.clientLibs = [
+  'systemjs/dist/system-polyfills.src.js',
+  'reflect-metadata/Reflect.js',
+  'es6-shim/es6-shim.js',
+  'systemjs/dist/system.src.js',
+  'angular2/bundles/angular2-polyfills.js',
+  'rxjs/bundles/Rx.js',
+  'angular2/bundles/angular2.js',
+  'angular2/bundles/router.js',
+  'angular2/bundles/http.js'
+];
+
+// load gulp plugins
+const $ = gulpLoadPlugins();
+
 gulp.task("default", ()=>{
   runSequence(
-    ["copy"]
+    ["dev"]
   )
 });
 
-// Copy files
-gulp.task("copy", ["copy:client", "copy:server"], ()=>{
+gulp.task("dev", ()=>{
+  runSequence(
+    'clean:dev',
+    'copy:dev',
+    'build:ts:dev',
+    'concat:lib'
+  );
+});
+
+gulp.task("clean:dev", ()=>{
+  del.sync(["**/*"], {cwd: CONFIG.dist});
+});
+
+
+//[Optional]
+gulp.task("tslint", ()=>{
 
 });
 
-gulp.task("copy:client", ()=>{
-  gulp.src(['**/*.html'], {cwd: CONFIG.client})
+// Copy files
+gulp.task("copy:dev", ["copy:client:dev", "copy:server:dev"], ()=>{
+
+});
+
+gulp.task("copy:client:dev", ()=>{
+  gulp.src(['**/*', "!**/*.ts", "!**/*.js", "!**/*.js.map", "!**/node_modules/**/*"], {cwd: CONFIG.client})
     .pipe(gulp.dest(CONFIG.public))
 });
 
-gulp.task("copy:server", ()=>{
+gulp.task("copy:server:dev", ()=>{
   gulp.src(["**/*", "package.json", ".babelrc"], {cwd: CONFIG.server})
     .pipe(gulp.dest(CONFIG.dist))
+});
+
+gulp.task("build:ts:dev", ()=>{
+  let _tsProject = $.typescript.createProject('tsconfig.json', {
+    typescript: require('typescript')
+  });
+  return gulp.src(["../../typings/browser.d.ts", "**/*.ts", "!**/*.spec.ts", "!**/*.e2e.ts", "!node_modules/**/*.ts"], {cwd: CONFIG.client})
+    .pipe($.typescript(_tsProject))
+    .pipe(gulp.dest(CONFIG.public));
+});
+
+gulp.task("concat:lib", ()=>{
+  gulp.src(CONFIG.clientLibs, {cwd: path.join(CONFIG.client, "node_modules")})
+    .pipe($.concat("lib.js"))
+    .pipe(gulp.dest(path.join(CONFIG.public, "js")))
+});
+
+gulp.task("uglify:lib", ()=>{
+  gulp.src(path.join(CONFIG.public, "js", "lib.js"))
+    .pipe($.uglify())
+    .pipe(gulp.dest(path.join(CONFIG.public, "js")));
+});
+
+gulp.task("build:index:dev", ()=>{
+
 });
