1c2a6665ca21f8f4da6bbc8266adf79a1deeba29
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-03-28 23:13:01 -0700

    Add list triggers UI

--- src/client/app/flogo.flows.detail.triggers/components/triggers.component.ts
+++ src/client/app/flogo.flows.detail.triggers/components/triggers.component.ts
@@ -1,5 +1,18 @@
 import {Component} from 'angular2/core';
 import {PostService} from '../../../common/services/post.service';
+import {TRIGGERS as TRIGGERS_MOCK} from '../mocks/triggers';
+
+export const PUB_EVENTS = {
+  LIST_TRIGGERS: {
+    channel:  'flogo-triggers',
+    topic:  'list-triggers'
+  },
+  LIST_TRIGGERS_DONE: {
+    channel:  'flogo-triggers',
+    topic:  'list-triggers-done'
+  }
+};
+
 
 @Component({
   selector: 'flogo-flows-detail-triggers',
@@ -7,13 +20,52 @@ import {PostService} from '../../../common/services/post.service';
   templateUrl: 'triggers.tpl.html',
   styleUrls: ['triggers.component.css']
 })
-
 export class FlogoFlowsDetailTriggers{
+  public triggers: any;
+  subscriptions:any;
   constructor(private _postService: PostService){
+    this.subscriptions = [];
     this.initSubscribe();
+
+    this._postService.publish(_.assign({},PUB_EVENTS.LIST_TRIGGERS));
+  }
+
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub:any) => {
+      this._postService.unsubscribe(sub);
+    });
   }
 
   private initSubscribe(){
 
+    this.subscriptions.push(this._postService.subscribe({
+      channel: PUB_EVENTS.LIST_TRIGGERS.channel,
+      topic: PUB_EVENTS.LIST_TRIGGERS.topic,
+      callback: function() {
+        this._listTriggers();
+      }.bind(this)
+    }));
+
+
+    // when the list of triggers arrive
+    let sub = {
+      channel: PUB_EVENTS.LIST_TRIGGERS_DONE.channel,
+      topic: PUB_EVENTS.LIST_TRIGGERS_DONE.topic,
+      callback:  (data:any, envelope:any) => {
+        this.triggers = data.triggers || [];
+      }
+    };
+
+    this.subscriptions.push(this._postService.subscribe(sub));
+
   }
+
+  private _listTriggers() {
+    let triggers =  TRIGGERS_MOCK;
+    var triggerDoneEvent = _.assign({}, PUB_EVENTS.LIST_TRIGGERS_DONE, {data:{triggers:triggers}});
+    this._postService.publish(triggerDoneEvent);
+
+  }
+
 }
---
+++ src/client/app/flogo.flows.detail.triggers/mocks/triggers.ts
@@ -0,0 +1,11 @@
+export const TRIGGERS =  [
+  {
+    "name": "http-trigger",
+    "title": "http Trigger"
+  },
+  {
+    "name": "mgtt-trigger",
+    "title": "mgtt Trigger"
+  }
+];
+
