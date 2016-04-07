820fe6cb98b721e384ac7648459b2eb9d9c62d5b
Yoyo <hyan@tibco-support.com>
2016-04-07 23:23:53 +0800

    Add delete button to every flows in homepage.

--- src/client/app/flogo.flows/components/flows.component.ts
+++ src/client/app/flogo.flows/components/flows.component.ts
@@ -18,9 +18,9 @@ export class FlogoFlowsComponet{
   ){
     this.getAllFlows();
   }
-
+  public flows:any[] = [];
   renderFlow( response, fail){
-    let className = 'flogo-flows-container-lists-success';
+   /* let className = 'flogo-flows-container-lists-success';
     let ng2StyleAttrReg = /^_ngcontent\-.*$/g;
     let eleAttrs = document.getElementById('flogo-flows-container-box').attributes;
     let ng2StyleAttr = (function () {
@@ -31,38 +31,52 @@ export class FlogoFlowsComponet{
       }
       return false;
     })();
-
+    let createTime = response.created_at?response.created_at: 'just now';
     if(fail){
       className = 'flogo-flows-container-lists-danger';
     }
     let html = `
     <li ${ng2StyleAttr} data-link="${response._id}" class="flogo-flows-container-list">
-      <div ${ng2StyleAttr} class="flogo-flows-container-list-name">
-        ${response.name}
-      </div>
-      <span ${ng2StyleAttr} class="flogo-flows-container-list-creatTime">${response.created_at}</span>
+      <a ${ng2StyleAttr} class="flogo-flows-container-list-detail" href="/flows/${response._id}">
+      <span ${ng2StyleAttr} class="flogo-flows-container-list-detail-trash" (click)="test()"></span>
+        <span ${ng2StyleAttr} class="flogo-flows-container-list-detail-name">${response.name}</span>
+        <span ${ng2StyleAttr} class="flogo-flows-container-list-detail-creatTime">${createTime}</span>
+      </a>
     </li>
     `;
     window.jQuery("#flogo-flows-container-box").addClass(className);
-    window.jQuery("#flogo-flows-container-lists").append(html);
+    window.jQuery("#flogo-flows-container-lists").append(html);*/
+  }
+  deleteFlow( id, rev) {
+    console.log(id, rev);
+    /*return false;*/
+      new Promise((resolve, reject)=> {
+          this._flow.deleteFlow(id, rev).then((response)=> {
+              console.log('remove flow successful' + response);
+              this.getAllFlows();
+              resolve(response);
 
+          }).catch((err)=> {
+              console.log('remove flow error' + err);
+              reject(err);
+          });
+      });
   }
   getAllFlows(){
     new Promise((resolve, reject)=>{
-      return new Promise((resolve, reject)=>{
-        this._flow.getFlows().then((response)=>{
-          if(typeof response !== 'object'){
-            response = JSON.parse(response);
-          }
-          console.log(response + response.length);
-          response.forEach((ele,index)=>{
-            this.renderFlow(ele);
-          });
-          resolve(response);
-        }).catch((err)=>{
-          this.renderFlow(err, true);
-          reject(err);
-        })
+      this._flow.getFlows().then((response)=>{
+        if(typeof response !== 'object'){
+          response = JSON.parse(response);
+        }
+        console.log(response + response.length);
+        this.flows = response;
+        /*response.forEach((ele,index)=>{
+         this.renderFlow(ele);
+         });*/
+        resolve(response);
+      }).catch((err)=>{
+        this.renderFlow(err, true);
+        reject(err);
       })
     })
   }
@@ -74,8 +88,7 @@ export class FlogoFlowsComponet{
         description: "Created by rest-api.spec"
       };
       this._flow.createFlow(_.clone(request)).then((response)=>{
-        console.log("create flow successful. ", response);
-        this.renderFlow(request);
+          this.getAllFlows();
         resolve(response);
       }).catch((err)=>{
         console.log("create flow error. ", err);
