99095b4d5fa4d1f7fc8394ca17c08645ef59feb0
Fabian Castillo <fcastill@tibco-support.com>
2016-05-12 12:26:54 -0500

    [aws-deploy] Stop process and state services correctly

--- aws-deploy/setup/stop-env.sh
+++ aws-deploy/setup/stop-env.sh
@@ -4,10 +4,12 @@
 pkill flogo
 
 ## Stop State Service
-ps ax | grep 'com.tibco.flogo.ss.service.FlogoServerService' | grep -v grep | awk '{ print "kill " -9 $1 }' | bash
+#ps ax | grep 'com.tibco.flogo.ss.service.FlogoServerService' | grep -v grep | awk '{ print "kill " -9 $1 }' | bash
+ps ax | grep 'flogo-internal/flow-state-service' | grep -v grep | awk '{ print "kill  -9 " $1 }' | bash
 
 ## Stop Flow Service
-ps ax | grep 'com.tibco.flogo.service.FlogoServerService' | grep -v grep | awk '{ print "kill " -9 $1 }' | bash
+#ps ax | grep 'com.tibco.flogo.service.FlogoServerService' | grep -v grep | awk '{ print "kill " -9 $1 }' | bash
+ps ax | grep 'flogo-internal/flow-service' | grep -v grep | awk '{ print "kill  -9 " $1 }' | bash
 
 ## Stop Redis
 #eval $(docker-machine env flogo)
