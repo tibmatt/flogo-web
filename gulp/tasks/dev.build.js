0b1fce2bb9c9534699a21c58a76c3c381d70f14d
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-06-02 17:46:36 -0500

    Add copy server files task, add install server and client install task

--- gulp/tasks/dev.build.js
+++ gulp/tasks/dev.build.js
@@ -1,13 +1,28 @@
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
-gulp.task('dev.build', cb => runSequence(
-  [
-    'dev.client.lib',
-    'dev.client.styles'
-  ],
-  'dev.client.typescript',
-  'dev.client.index',
-  'dev.client.assets',
-  cb
-));
+gulp.task('dev.build', ['dev.client.build','dev.server.build', 'dev.install']);
+
+
+gulp.task('dev.client.build', (cb)=> {
+  return runSequence(
+    [
+      'dev.client.lib',
+      'dev.client.styles'
+    ],
+    'dev.client.typescript',
+    'dev.client.index',
+    'dev.client.assets',
+    cb
+  )
+});
+
+
+gulp.task('dev.server.build', (cb)=> {
+
+  return runSequence([
+    'dev.server.copy'
+  ],cb)
+
+
+});
---
+++ gulp/tasks/dev.server.copy.js
@@ -0,0 +1,8 @@
+import gulp from 'gulp';
+import {CONFIG} from '../config';
+
+
+gulp.task("dev.server.copy", ()=>{
+  return gulp.src(["**/*", "package.json", "!**/node_modules/**"], {cwd: CONFIG.paths.source.server})
+    .pipe(gulp.dest(CONFIG.paths.dist.server))
+});
