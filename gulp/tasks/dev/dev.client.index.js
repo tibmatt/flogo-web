54c0b802fcf1239aaddec0e39e2439bd31c8d4a3
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-06-07 16:55:47 -0500

    Fixed path issue when index.html load lib files.

--- gulp/tasks/dev/dev.client.index.js
+++ gulp/tasks/dev/dev.client.index.js
@@ -16,5 +16,5 @@ gulp.task('dev.client.index', () => {
 function injectJsLibs() {
   let jsLibsPath = path.join(CONFIG.paths.source.client);
   let sources = gulp.src(CONFIG.libs.js, {cwd: jsLibsPath});
-  return inject(sources, {relative: true, addPrefix: 'js'});
+  return inject(sources, {relative: true, addPrefix: '/js'});
 }
