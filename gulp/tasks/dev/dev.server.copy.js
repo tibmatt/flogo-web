f94a7a523c79c64db94710787261c2cf4f86d49d
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-06-07 12:06:38 -0500

    Copy only Koa server changed files to dist server.

--- gulp/tasks/dev/dev.server.copy.js
+++ gulp/tasks/dev/dev.server.copy.js
@@ -1,8 +1,10 @@
 import gulp from 'gulp';
+import changed from 'gulp-changed';
 
 import {CONFIG} from '../../config';
 
 gulp.task('dev.server.copy', () => {
   return gulp.src(["**/*", "package.json", "!**/node_modules/**"], {cwd: CONFIG.paths.source.server})
+    .pipe(changed(CONFIG.paths.dist.server))
     .pipe(gulp.dest(CONFIG.paths.dist.server));
 });
