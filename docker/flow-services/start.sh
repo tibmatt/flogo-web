2e3093d49945b43d3d13d2773004f79c553137aa
Fabian Castillo <fcastill@tibco-support.com>
2016-06-17 11:16:05 -0500

    Added docker compose file and dockerfile for state and flow services

---
+++ docker/flow-services/start.sh
@@ -0,0 +1,2 @@
+cd $FLOGO_FLOW_SERVICE/build/install/flow-service/bin && bash flow-service server ../config/server.yml &
+cd $FLOGO_FLOW_STATE_SERVICE/build/install/state-service/bin && bash state-service server ../config/server.yml
