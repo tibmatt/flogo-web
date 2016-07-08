53513a98a56878af4cdd46407ee95d3746edb546
Fabian Castillo <fcastill@tibco-support.com>
2016-07-08 16:02:14 -0500

    Remove unused files.

--- docker/build-flogo-cli.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-flogoCliDockerFile="docker/flogo-cli/Dockerfile"
-cd ..
-docker build -t "flogo/flogo-cli" --file="$flogoCliDockerFile" .
--- docker/build-services.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-SUBMODULES_DIR=../submodules
-cd $SUBMODULES_DIR/flogo-internal/docker
-
-./build.sh flow-service
-./build.sh flow-state-service
