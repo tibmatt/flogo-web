3252c301288de525ae5356a4ebd67c68505955fd
Fabian Castillo <fcastill@tibco-support.com>
2016-07-13 17:25:03 -0500

    Make optional activity install skip in gulp restart and increase nodemon verbosity

--- gulp/tasks/dev/dev.start.js
+++ gulp/tasks/dev/dev.start.js
@@ -13,12 +13,15 @@ gulp.task('dev.start', 'Starts server app and db for development', () => {
 
   cp.spawn('npm', ['run', 'start-db'], {cwd: CONFIG.paths.dist.server, stdio: 'inherit'});
   nodemon({
+    verbose: true,
     // DON'T use cwd here, it will change the whole gulp process cwd
     exec: `npm --prefix="${CONFIG.paths.dist.server}" run start-dev-server`,
     watch: CONFIG.paths.serverWatch.map(watchPath => path.join(CONFIG.paths.dist.server, watchPath))
   })
   .on('start', () => {
-    process.env['FLOGO_SKIP_PKG_INSTALL'] = true;
+    if(process.env['FLOGO_SKIP_PKG_INSTALL_ON_RESTART']) {
+      process.env['FLOGO_SKIP_PKG_INSTALL'] = true;
+    }
   });
 
 });
