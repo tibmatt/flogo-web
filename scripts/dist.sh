9bb890970cf4c8e9eb47da13b39bad67efb82232
Fabian Castillo <fcastill@tibco-support.com>
2016-07-18 13:50:05 -0500

    Remove libs after use in dockerfile

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+DIST_BRANCH="$1"
+
 cd ..
 rm -rf dist
 mkdir dist
 
-git clone -b 0.2alpha1 --single-branch https://github.com/TIBCOSoftware/flogo-web.git dist
+git clone -b "${DIST_BRANCH}" --single-branch https://github.com/TIBCOSoftware/flogo-web.git dist
 rm -rf dist/build/*
 
 mkdir -p dist/submodules
 cp -r submodules/flogo-contrib dist/submodules
 
 DIST_BUILD=true gulp dist
+
+rm -rf dist/submodules
