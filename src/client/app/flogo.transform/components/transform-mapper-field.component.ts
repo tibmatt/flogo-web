a85c2342a71c3540638c1adced707fa07a1bfe3d
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-07-23 08:40:59 -0500

    Show validation errors.

--- src/client/app/flogo.transform/components/transform-mapper-field.component.ts
+++ src/client/app/flogo.transform/components/transform-mapper-field.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter, OnChanges, OnInit } from '@angular/core';
+import { mappingsValidateField } from '../validators/validators';
 
 @Component({
     selector: 'flogo-transform-mapper-field',
@@ -12,17 +13,42 @@ export class TransformMapperField implements  OnChanges, OnInit {
     @Input() mappings:any;
     @Input() precedingTilesOutputs:any;
     @Input() tileInputInfo:any;
+    @Input() tileInfo:any;
     selectedValue:string = '';
     showList:boolean  = false;
     tiles:any[] = [];
     hasError:boolean = false;
+    messageError:string = '';
 
 
     ngOnInit() {
     }
 
-    onChangeText(event) {
-        debugger;
+    onKeyUp(target:any) {
+        this.validateField(target.value);
+    }
+
+    validateField(value:string) {
+        let errors:any = mappingsValidateField(this.tileInfo, this.wrapInJSON(value));
+        if(errors&&errors.invalidMappings) {
+            this.setError(true,this.getErrorMessage(errors.invalidMappings.errors || []) )
+        }else {
+            this.setError(false, '');
+        }
+    }
+
+    setError(hasError:boolean, message:string) {
+        this.hasError = hasError;
+        this.messageError  = message;
+    }
+
+    getErrorMessage(errors:any[]) {
+        let message = '';
+
+        errors.forEach(function (error) {
+            message += 'Invalid field \n' + error.value.value + ' mapping to ' + error.value.mapTo;
+        });
+        return message;
     }
 
     precedingTilesToArray(precedingTiles:any) {
@@ -37,7 +63,6 @@ export class TransformMapperField implements  OnChanges, OnInit {
     }
 
     ngOnChanges(changes:any) {
-        debugger;
 
         if(changes.precedingTilesOutputs && this.tiles) {
             this.tiles = this.precedingTilesToArray(changes.precedingTilesOutputs.currentValue);
@@ -45,8 +70,8 @@ export class TransformMapperField implements  OnChanges, OnInit {
 
     }
 
-    focusInputText() {
-        this.showList = true;
+    toggleList() {
+        this.showList = !this.showList;
     }
 
     onKeyPress(event) {
@@ -57,12 +82,19 @@ export class TransformMapperField implements  OnChanges, OnInit {
 
     clickField(output, field) {
         this.selectedValue = output.name + '.' + field.name;
+        this.validateField(this.selectedValue);
         this.showList = false;
     }
 
     clickRemove() {
         this.selectedValue = '';
         this.showList = false;
+        this.setError(false, '');
+    }
+
+    wrapInJSON(value) {
+        return '[ { "type": 1, "value": "' + value + '", "mapTo": "' + this.tile.name + '" } ]'
+
     }
 
 }
