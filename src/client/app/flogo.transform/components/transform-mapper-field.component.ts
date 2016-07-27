347ec22d623e9f51d9a092994371465c57a74b70
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-07-27 11:23:31 -0500

    Hide menu tile previous list when mouse leave.

--- src/client/app/flogo.transform/components/transform-mapper-field.component.ts
+++ src/client/app/flogo.transform/components/transform-mapper-field.component.ts
@@ -33,7 +33,7 @@ export class TransformMapperField implements  OnChanges, OnInit {
     }
 
     onMouseLeave() {
-        //this.resetStatus();
+        this.resetStatus();
     }
 
     resetStatus() {
