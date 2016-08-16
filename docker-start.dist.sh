b655fe2d57194d5f44b5412d874f6fa148335df4
Fabian Castillo <fcastill@tibco-support.com>
2016-08-16 11:59:34 -0500

    Update docker files from dist branch

--- docker-start.dist.sh
+++ docker-start.dist.sh
@@ -4,20 +4,6 @@
 echo "Setting up git"
 cd /tmp/flogo-web
 
-
-## setup GIT
-#echo "Username is $GIT_USERNAME and PASSWORD is $GIT_PASSWORD"
-#git config --global credential.helper store
-#git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@github.com/TIBCOSoftware/flogo-contrib.git
-#git remote add origin https://$GIT_USERNAME:$GIT_PASSWORD@github.com/TIBCOSoftware/flogo-cli.git
-
-## Go get Flogo-CLI
-#echo "Getting flogo-cli"
-#cd $GOPATH/src
-#echo "CLONING flogo-cli"
-#git clone https://$GIT_USERNAME:$GIT_PASSWORD@github.com/TIBCOSoftware/flogo-cli.git
-#go get -u github.com/TIBCOSoftware/flogo-cli/...
-
 #############################
 # start flogo-web
 #############################
