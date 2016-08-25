dad8dce80b3a9c62fe90d0fc2cfc74c5b989be6f
Fabian Castillo <fcastill@tibco-support.com>
2016-08-25 07:21:26 -0500

    Replace flogo-cli clone with docker image

--- scripts/release-build.sh
+++ scripts/release-build.sh
@@ -33,8 +33,9 @@ docker::pull_and_tag "flogo/flogo-contrib:latest"
 
 cid=$(docker create --name contrib-container flogo/flogo-contrib)
 docker run --rm --volumes-from contrib-container \
+      # -u 1000:1000 \
       -v ${BUILD_ROOT}/dist/submodules:/submodules \
-      alpine /bin/sh -c "ls -al /flogo/flogo-contrib && cp -r /flogo/flogo-contrib /submodules"
+      alpine /bin/sh -c "ls -al /flogo/flogo-contrib && cp -r /flogo/flogo-contrib /submodules && chown -R 1000:1000 /submodules"
 docker rm -f ${cid}
 # cp -r ${BUILD_ROOT}/submodules/flogo-contrib ${BUILD_ROOT}/dist/submodules
 
@@ -45,20 +46,16 @@ DIST_BUILD=true npm run start release
 rm -rf ${BUILD_ROOT}/dist/submodules
 rm -rf ${BUILD_ROOT}/dist/build/server/node_modules
 
-# add flogo-cli source code to avoid pulling in from inside the container so we don't need to provide credentials for the private repo
-# TODO: remove once flogo-cli repo is public
-git clone --single-branch https://github.com/TIBCOSoftware/flogo-cli.git ${BUILD_ROOT}/dist/flogo-cli
-rm -rf ${BUILD_ROOT}/dist/flogo-cli/.git
-
 #Replace git clone flogo-cli above with the following
 # Pull and tag flogo/flogo-cli
-# docker::pull_and_tag "flogo/flogo-cli:latest"
+docker::pull_and_tag "flogo/flogo-cli:latest"
 
-# cid=$(docker create --name cli-container flogo/flogo-cli)
-# docker run --rm --volumes-from cli-container \
-#       -v ${BUILD_ROOT}/dist/submodules:/submodules \
-#       alpine /bin/sh -c "ls -al /flogo/flogo-cli && cp -r /flogo/flogo-cli /submodules"
-# docker rm -f ${cid}
+cid=$(docker create --name cli-container flogo/flogo-cli)
+docker run --rm --volumes-from cli-container \
+       # -u 1000:1000 \
+       -v ${BUILD_ROOT}/dist:/flogo-web \
+       alpine /bin/sh -c "ls -al /flogo/flogo-cli && cp -r /flogo/flogo-cli /flogo-web"
+docker rm -f ${cid}
 
 
 # Build flogo/flogo-web docker image
