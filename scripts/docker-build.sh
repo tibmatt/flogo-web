99bd6680d3b1073a874c166f29631e43971f7f4e
Pranab Dhar <pdhar@tibco.com>
2016-08-25 13:44:26 -0700

    isolate travis vars to common.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,8 +5,8 @@ docker::build_and_push() {
 
   local image_name="${1}"
   local file_name="${2:-Dockerfile}"
-  local bid="$BID"
-  local branch="${TRAVIS_BRANCH}"
+  local bid="${BUILD_NUMBER}"
+  local branch="${BUILD_BRANCH}"
 
   docker::build_file "${file_name}" "${image_name}" "${bid}" && \
   docker::push_and_tag "${image_name}" "${bid}" "${branch}"
@@ -17,30 +17,32 @@ docker::build() {
 }
 
 docker::build_file() {
+  common::envvars
+
   local file_name="$1"
   local image_name="$2"
-  local bid="$3"
-   
-  local buildTypeId="${TRAVIS_BUILD_ID}"
+  local bid="${3:-${BUILD_NUMBER}}"
+  local branch="${BUILD_BRANCH}"
+  local buildUrl="${BUILD_URL}" 
+  local gitUrl="${BUILD_GIT_URL}"
+  local buildTypeId="${BUILD_TYPE_ID}"
+  local gitCommit="${BUILD_GIT_COMMIT}"
 
   if [ -n "$buildTypeId" ]; then
       cp ${file_name} ${file_name}_backup
-      GIT_REPO=$(git config --get remote.origin.url)
       echo "" >> ${file_name}
       echo "LABEL com.tibco.flogo.ci.buildNumber=\"${bid}\" \\" >> ${file_name}
       echo " com.tibco.flogo.ci.buildTypeId=\"${buildTypeId}\" \\" >> ${file_name}
-      echo " com.tibco.flogo.ci.url=\"https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${buildTypeId}\"" >> ${file_name}
-      # since TeamCity supports multiple VCS roots, there is no default for GIT_REPO and GIT_COMMIT
-      if [ -n "$GIT_REPO" ]; then
-          echo "LABEL com.tibco.flogo.git.repo=\"$GIT_REPO\" com.tibco.flogo.git.commit=\"${TRAVIS_COMMIT}\"" >> ${file_name}
+      echo " com.tibco.flogo.ci.url=\"${buildUrl}\"" >> ${file_name}
+      if [ -n "${branch}" ]; then
+          echo "LABEL com.tibco.flogo.git.repo=\"${gitUrl}\" com.tibco.flogo.git.commit=\"${gitCommit}\"" >> ${file_name}
       fi
   fi
 
   local latest='latest'
-  # non-branch-aware Travis jobs won't have $TRAVIS_BRANCH at all
-  if [[ -n ${TRAVIS_BRANCH} && ( ${TRAVIS_BRANCH} != 'master' ) ]]; then
-    latest="latest-$branch"
-    bid="$bid-$branch"
+  if [[ -n ${branch} && ( ${branch} != 'master' ) ]]; then
+    latest="latest-${branch}"
+    bid="${bid}-${branch}"
   fi
 
   local latest='latest'
@@ -61,38 +63,37 @@ docker::build_file() {
 
 docker::pull_and_tag() {
   local base_name="$1"
-  local docker_registry="$DOCKER_REGISTRY"
+  local docker_registry=$( [ -n "${DOCKER_REGISTRY}" ] && echo "${DOCKER_REGISTRY}/" || echo "" ) 
 
   if [ -n "$docker_registry" ]; then
-    docker pull $docker_registry/$base_name && \
-    docker tag $docker_registry/$base_name $base_name && \
-    docker rmi $docker_registry/$base_name
+    docker pull ${docker_registry}${base_name} && \
+    docker tag ${docker_registry}${base_name} ${base_name} && \
+    docker rmi ${docker_registry}${base_name}
   fi
 }
 
 docker::push_and_tag() {
   common::envvars
   local image_name="$1"
-  local bid="$2"
-  local docker_registry="$DOCKER_REGISTRY"
-
+  local bid="${2:-${BUILD_NUMBER}}"
+  local docker_registry=$( [ -n "${DOCKER_REGISTRY}" ] && echo "${DOCKER_REGISTRY}/" || echo "" ) 
+  local branch="${BUILD_BRANCH}"
   local latest='latest'
-  # non-branch-aware Travis jobs won't have $TRAVIS_MASTER at all
-  if [[ -n ${TRAVIS_BRANCH} && ( ${TRAVIS_BRANCH} != 'master' ) ]]; then
-    latest="latest-$branch"
-    bid="$bid-$branch"
+  if [[ -n ${branch} && ( ${branch} != 'master' ) ]]; then
+    latest="latest-${branch}"
+    bid="${bid}-${branch}"
   fi
 
-  if [ -n "$bid" -a -n "$docker_registry" ]; then
+  if [ -n "${bid}" -a -n "${branch}" ]; then
     echo "Publishing image..."
-    docker tag $image_name:$bid $image_name:$latest && \
-    docker tag $image_name:$bid $docker_registry/$image_name:$bid && \
-    docker tag $image_name:$bid $docker_registry/$image_name:$latest && \
-    docker images | grep $image_name >> images.txt && \
-    docker push $docker_registry/$image_name:$latest && \
-    docker push $docker_registry/$image_name:$bid && \
-    docker rmi $docker_registry/$image_name:$latest && \
-    docker rmi $docker_registry/$image_name:$bid && \
+    docker tag ${image_name}:${bid} ${image_name}:${latest} && \
+    docker tag ${image_name}:${bid} ${docker_registry}${image_name}:${bid} && \
+    docker tag ${image_name}:${bid} ${docker_registry}${image_name}:${latest} && \
+    docker images | grep ${image_name} >> images.txt && \
+    docker push ${docker_registry}${image_name}:${latest} && \
+    docker push ${docker_registry}${image_name}:${bid} && \
+    docker rmi ${docker_registry}${image_name}:${latest} && \
+    docker rmi ${docker_registry}${image_name}:${bid} && \
     echo "Done."
   fi
 }
@@ -102,26 +103,26 @@ docker::copy_tag_and_push() {
   common::envvars
   local src_image_name="$1"
   local dest_image_name="$2"
-  local bid="$3"
-  local docker_registry="${DOCKER_REGISTRY}"
-
+  local bid="${3:-${BUILD_NUMBER}}"
+  local docker_registry=$( [ -n "${DOCKER_REGISTRY}" ] && echo "${DOCKER_REGISTRY}/" || echo "" ) 
+  local branch="${BUILD_BRANCH}"
   local latest='latest'
   # non-branch-aware TeamCity jobs won't have $IS_MASTER at all
-  if [[ -n ${TRAVIS_BRANCH} && ( ${TRAVIS_BRANCH} != 'master' ) ]]; then
-    latest="latest-$branch"
-    bid="$bid-$branch"
+  if [[ -n ${branch} && ( ${branch} != 'master' ) ]]; then
+    latest="latest-${branch}"
+    bid="${bid}-${branch}"
   fi
   
 
-  if [ -n "${bid}" -a -n "${docker_registry}" ]; then
+  if [ -n "${bid}" -a -n "${branch}" ]; then
     echo "Retagging image from: ${src_image_name}:${bid} to: ${dest_image_name}:${bid} ..."
     docker tag ${src_image_name}:${bid} ${dest_image_name}:${latest} && \
-    docker tag ${src_image_name}:${bid} ${docker_registry}/${dest_image_name}:${bid} && \
-    docker tag ${src_image_name}:${bid} ${docker_registry}/${dest_image_name}:${latest} && \
-    docker push ${docker_registry}/${dest_image_name}:${latest} && \
-    docker push ${docker_registry}/${dest_image_name}:${bid} && \
-    docker rmi ${docker_registry}/${dest_image_name}:${latest} && \
-    docker rmi ${docker_registry}/${dest_image_name}:${bid} && \
+    docker tag ${src_image_name}:${bid} ${docker_registry}${dest_image_name}:${bid} && \
+    docker tag ${src_image_name}:${bid} ${docker_registry}${dest_image_name}:${latest} && \
+    docker push ${docker_registry}${dest_image_name}:${latest} && \
+    docker push ${docker_registry}${dest_image_name}:${bid} && \
+    docker rmi ${docker_registry}${dest_image_name}:${latest} && \
+    docker rmi ${docker_registry}${dest_image_name}:${bid} && \
     echo "Done."
   else
      # no bid and docker registry i.e. local machine
