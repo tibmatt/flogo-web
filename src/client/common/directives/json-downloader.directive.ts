3fe31c0c8aa22fd3c8a31b374c5e52672496f4f3
David Cocom <dcocomba@tibco-support.com>
2016-09-14 11:50:11 -0500

    File content is open in a new tab if 'download' is not available

--- src/client/common/directives/json-downloader.directive.ts
+++ src/client/common/directives/json-downloader.directive.ts
@@ -31,6 +31,10 @@ export class JsonDownloader {
 
         this._link.setAttribute('href', dataString);
         this._link.setAttribute('download', 'flow.json');
+        //if download attribute is not supported
+        if (!('download' in this._link)) {
+          this._link.setAttribute('target', '_blank');
+        }
         document.body.appendChild(this._link);
         this._link.click();
         document.body.removeChild(this._link);
