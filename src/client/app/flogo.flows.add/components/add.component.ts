17664eef91a8d157711e9087c71185ba05268e0b
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-10-26 12:26:22 -0500

    Add feature in create flow modal to show an error if the name flow exists in database.

--- src/client/app/flogo.flows.add/components/add.component.ts
+++ src/client/app/flogo.flows.add/components/add.component.ts
@@ -3,6 +3,7 @@ import {PostService} from '../../../common/services/post.service';
 import {PUB_EVENTS} from '../message';
 import {MODAL_DIRECTIVES, ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
 import { TranslatePipe, TranslateService } from 'ng2-translate/ng2-translate';
+import { RESTAPIFlowsService } from '../../../common/services/restapi/flows-api.service';
 
 @Component({
     selector: 'flogo-flows-add',
@@ -18,27 +19,48 @@ export class FlogoFlowsAdd {
 
     private flowInfo : any = {};
     private _sending = true;
+    public flowNameExists = false;
 
-    constructor(private _postService: PostService, public translate: TranslateService) {
+    constructor(private _postService: PostService, public translate: TranslateService, public APIFlows: RESTAPIFlowsService) {
     }
 
     @ViewChild('modal')
         modal: ModalComponent;
 
     public sendAddFlowMsg() {
-        if (this._sending) {
-            this._sending = false;
-            this._postService.publish(
-                _.assign({}, PUB_EVENTS.addFlow, {data: this.flowInfo})
-            );
-            this.closeAddFlowModal();
-        } else {
-            // omit
-        }
+        this.APIFlows.getFlowByName(this.flowInfo.name)
+            .then((res) => {
+                this.flowNameExists = true;
+            })
+            .catch((err) => {
+                // flow name doesn't exists
+                if(err.status == 400) {
+                    this.flowNameExists = false;
+                    if (this._sending) {
+                        this._sending = false;
+                        this._postService.publish(
+                            _.assign({}, PUB_EVENTS.addFlow, {data: this.flowInfo})
+                        );
+                        this.closeAddFlowModal();
+                    } else {
+                        // omit
+                    }
+                }
+
+            });
+
+
+
     }
     private closeAddFlowModal() {
         this.flowInfo = {};
         this.modal.close();
         this._sending = true;
+        this.flowNameExists = false;
     }
+
+    public onChangeFlowName(event) {
+        this.flowNameExists = false;
+    }
+
 }
