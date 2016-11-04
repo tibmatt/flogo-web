c15549ee2a3c432f260e183f3bef5cb246f8b797
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-11-04 11:30:47 -0600

    Init  logger to monitor the winston log file.

--- src/server/common/logger.js
+++ src/server/common/logger.js
@@ -1,16 +1,28 @@
 import path from 'path';
-
 import winston from 'winston';
+import { runShellCMD } from '../common/utils'
 
 import {config} from '../config/app-config';
 
 // TODO: use getLogger(loggerIdentifier) ex: getLogger('testEngine')
 
-export const engineLogger = new winston.Logger({
+var logger = new winston.Logger({
   level: 'debug',
   transports: [
     new winston.transports.File({ filename: path.join(config.rootPath, 'winston.log') })
   ]
 });
+
+runShellCMD( 'npm', ['run','start-logger'], {} )
+  .then((result)=> {
+    console.log('start-logger started')
+  })
+  .catch((err)=> {
+      console.log('Error starting start-logger:');
+      console.log(err);
+  });
+
+
+export const engineLogger = logger;
 
 
