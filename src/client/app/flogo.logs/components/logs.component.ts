a1e751e38ac1aa08638988ec9abe97121a701d56
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-11-08 10:53:28 -0600

    Display , date, level and error message in log control.

--- src/client/app/flogo.logs/components/logs.component.ts
+++ src/client/app/flogo.logs/components/logs.component.ts
@@ -13,12 +13,39 @@ import { TranslatePipe } from 'ng2-translate/ng2-translate';
   }
 )
 export class FlogoLogs {
+    messages: string[];
 
   constructor( ) {
+      this.messages = [];
+      var host = 'localhost';
+      var ws = new WebSocket('ws://' + host + ':3011');
+      ws.onmessage = (event) => {
+          console.info('Received message', new Date());
+          try {
+              var data = JSON.parse(event.data);
+              this.appendLog(data);
+          } catch(e) {
+              console.error(e);
+          }
+      };
   }
 
 
   ngOnDestroy() {
+  }
+
+  appendLog(logData) {
+      let message = {
+          level: logData.level,
+          timestamp: new Date(logData.timestamp).toString().substring(0,25),
+          message: logData.message
+      }
+
+      this.messages.push(message);
+    //message = message.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    //if(window.ansi_up) {
+    //    message = ansi_up.ansi_to_html(message);
+    //}
   }
 
 
