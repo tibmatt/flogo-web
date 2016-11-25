bc063bca6685a7082d2f09f3ef7554342efed3ff
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2016-11-24 18:05:24 -0600

    Change layout of logs panel to fixed.

--- src/client/app/flogo.logs/components/logs.component.ts
+++ src/client/app/flogo.logs/components/logs.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, ElementRef, Renderer } from '@angular/core';
 import { TranslatePipe } from 'ng2-translate/ng2-translate';
 import { PostService } from '../../../common/services/post.service';
 import { LogService } from '../../../common/services/log.service';
 import { SearchPipe } from './search.component';
 import { PUB_EVENTS } from '../messages';
 
+const MAXIMIZED_WIDTH = '800px';
 @Component(
   {
     selector : 'flogo-logs',
@@ -20,7 +21,7 @@ export class FlogoLogs {
     searchValue: string = '';
     isMaximized: boolean = false;
 
-  constructor(public logService: LogService, public postService: PostService ) {
+  constructor(public elRef:ElementRef, private renderer:Renderer, public logService: LogService, public postService: PostService ) {
   }
 
   public onKeyUp(event) {
@@ -29,6 +30,12 @@ export class FlogoLogs {
 
   public resizePanel(event,action)   {
       this.isMaximized = (action === 'grow');
+      if(this.isMaximized) {
+          this.renderer.setElementStyle(this.elRef.nativeElement, 'width', MAXIMIZED_WIDTH);
+      }else {
+          this.renderer.setElementStyle(this.elRef.nativeElement, 'width', '');
+      }
+
       this.postService.publish(
           _.assign({}, PUB_EVENTS.logResize, {data: {action:action}})
       )
