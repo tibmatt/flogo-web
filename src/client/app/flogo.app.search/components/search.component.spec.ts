e4b3ae5080248d2736fc13a9176430f6da030be3
Samuel Zuniga Vanoye <szunigav@tibco-support.com>
2017-01-03 18:29:29 -0600

    Add unit test to FlogoApplicationSearch component to verify the changedSearch event is emitted when user press a key.

---
+++ src/client/app/flogo.app.search/components/search.component.spec.ts
@@ -0,0 +1,51 @@
+import { describe, beforeEach, beforeEachProviders, it, inject, expect, injectAsync} from '@angular/core/testing';
+import { FlogoApplicationSearch  } from './search.component';
+import { TestComponentBuilder } from '@angular/compiler/testing';
+import { TranslateService, TranslateLoader } from 'ng2-translate/ng2-translate';
+import { By } from '@angular/platform-browser';
+import { IFlogoApplicationModel } from '../../../common/application.model';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+
+describe('FlogoApplicationSearch component', () => {
+    let tcb: TestComponentBuilder;
+
+    function createComponent() {
+        return tcb.createAsync(FlogoApplicationSearch);
+    }
+
+    //setup
+    beforeEachProviders(()=> [
+        HTTP_PROVIDERS,
+        TestComponentBuilder,
+        TranslateService,
+        TranslateLoader,
+        FlogoApplicationSearch
+    ]);
+
+    beforeEach(inject([TestComponentBuilder], (_tcb:TestComponentBuilder) => {
+        tcb = _tcb;
+    }));
+
+
+
+    it('On key press should emit the changedSearch event', done => {
+        createComponent()
+            .then(fixture => {
+                let searchComponent = fixture.componentInstance;
+
+                searchComponent.changedSearch.subscribe(() => {
+                    done();
+                });
+
+                fixture.detectChanges();
+                let input = fixture.debugElement.query(By.css('input'));
+
+                let element = input.nativeElement;
+                let event = new Event('keyup');
+                element.dispatchEvent(event);
+            });
+    });
+
+
+});
