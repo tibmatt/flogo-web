df4745f65e040684711608a9102b120d95361347
David Cocom <dcocomba@tibco-support.com>
2017-01-10 14:15:11 -0600

    Get apps from API service

--- src/client/common/services/restapi/applications-api.service.ts
+++ src/client/common/services/restapi/applications-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
-import { IFlogoApplicationModel } from '../../../common/application.model';
+import { IFlogoApplicationModel } from '../../application.model';
 
 const UNTITLED_APP = 'Untitled App';
 
@@ -131,9 +131,17 @@ export class RESTAPIApplicationsService {
   }
 
   list() {
-      return new Promise((resolve, reject)=> {
-          resolve(this.applications);
-      });
+    return new Promise((resolve, reject) => {
+      this._http.get('/v1/api/apps').toPromise()
+        .then(response => {
+          if (response.text()) {
+            let apps: Array<IFlogoApplicationModel> = response.json().data;
+            resolve(apps);
+          } else {
+            reject(response)
+          }
+        });
+    });
   }
 
   allFlows()   {
