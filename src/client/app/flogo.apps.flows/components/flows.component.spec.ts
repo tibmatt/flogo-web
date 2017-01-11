cc674872562adc13791648997d1553e734cf9bea
Anand Pandurangi <apandura@tibco.com>
2017-01-11 15:23:00 +0530

    Fixed broken test of flogo.apps.flows/component/flows.component.ts
    Signed-off-by: Anand Pandurangi <apandura@tibco.com>

--- src/client/app/flogo.apps.flows/components/flows.component.spec.ts
+++ src/client/app/flogo.apps.flows/components/flows.component.spec.ts
@@ -1,100 +1,107 @@
-// import {Component, Output, EventEmitter} from '@angular/core';
-// import { FlogoApplicationFlowsComponent } from './flows.component';
-// import { describe, beforeEach, beforeEachProviders, it, inject, expect, injectAsync } from '@angular/core/testing';
-// import { HTTP_PROVIDERS } from '@angular/http';
-// import { TestComponentBuilder } from '@angular/compiler/testing';
-// import { TranslateService, TranslateLoader, TranslateStaticLoader } from 'ng2-translate/ng2-translate';
-// import { By } from '@angular/platform-browser';
-//
-// @Component({
-//     selector: 'container',
-//     template: `
-//                 <div class="flows">
-//                     <flogo-app-flows [flows]="flows"></flogo-app-flows>
-//                 </div>
-//             `,
-//     directives: [FlogoApplicationFlowsComponent]
-// })
-// export class Container {
-//     @Output() changes = new EventEmitter();
-//     flows : Array<any> = [
-//         {
-//             id: '897',
-//             name: 'Manually adjust temperature',
-//             description: 'A flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt Luptas oilsksd as asdfwo',
-//             createdAt: new Date()
-//         },
-//         {
-//             id: '987',
-//             name: 'Raise temperature & notifiy operator',
-//             description: 'A basic flow for apietusam',
-//             createdAt: new Date()
-//         },
-//         {
-//             id: '879',
-//             name: 'Log temperature',
-//             description: 'A complex flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt',
-//             createdAt: new Date()
-//         },
-//         {
-//             id: '869',
-//             name: 'Log temperature 2',
-//             description: 'A complex flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt',
-//             createdAt: new Date()
-//         }
-//     ];
-//
-//     // helper function to repeat the event propagation
-//     changed(value, property) {
-//         this.changes.emit(value);
-//     }
-// }
-//
-// describe('Application flows', ()=> {
-//     let fixture, tcb: TestComponentBuilder;
-//
-//     // setup
-//     beforeEachProviders(()=>[
-//         HTTP_PROVIDERS,
-//         TestComponentBuilder,
-//         Container,
-//         TranslateService,
-//         TranslateLoader,
-//     ]);
-//
-//     beforeEach(injectAsync([TestComponentBuilder], (_tcb: TestComponentBuilder) => {
-//         tcb = _tcb;
-//     }));
-//
-//
-//     it('Should render 4 flows', done => {
-//         tcb.createAsync(Container)
-//             .then(fixture => {
-//                 fixture.detectChanges();
-//                 let flows = fixture.debugElement.queryAll(By.css('.flows-container > .flow'));
-//                 expect(flows.length).toEqual(4);
-//                 done();
-//             });
-//     });
-//
-//     it('Should display the flow title', done => {
-//         tcb.createAsync(Container)
-//             .then(fixture => {
-//                 fixture.detectChanges();
-//                 let title = fixture.debugElement.query(By.css('.title:nth-of-type(1)'));
-//                 expect(title.nativeElement.innerText).toEqual('Manually Adjust Temperature');
-//                 done();
-//             });
-//     });
-//
-//     it('Should display the flow description', done => {
-//         tcb.createAsync(Container)
-//             .then(fixture => {
-//                 fixture.detectChanges();
-//                 let description = fixture.debugElement.queryAll(By.css('.description'));
-//                 expect(description[1].nativeElement.innerText).toEqual('A basic flow for apietusam');
-//                 done();
-//             });
-//     });
-//
-// });
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {Component, Output, EventEmitter, DebugElement}    from '@angular/core';
+import {TranslateModule, TranslateLoader, TranslateStaticLoader} from 'ng2-translate/ng2-translate';
+import {ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
+import {Http} from '@angular/http';
+
+import {FlogoApplicationFlowsComponent} from './flows.component';
+
+@Component({
+  selector: 'container',
+  template: `
+                <div class="flows">
+                    <flogo-app-flows [flows]="flows"></flogo-app-flows>
+                </div>
+            `,
+  directives: [FlogoApplicationFlowsComponent]
+})
+class Container {
+  @Output() changes = new EventEmitter();
+  flows: Array<any> = [
+    {
+      id: '897',
+      name: 'Manually adjust temperature',
+      description: 'A flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt Luptas oilsksd as asdfwo',
+      createdAt: new Date()
+    },
+    {
+      id: '987',
+      name: 'Raise temperature & notifiy operator',
+      description: 'A basic flow for apietusam',
+      createdAt: new Date()
+    },
+    {
+      id: '879',
+      name: 'Log temperature',
+      description: 'A complex flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt',
+      createdAt: new Date()
+    },
+    {
+      id: '869',
+      name: 'Log temperature 2',
+      description: 'A complex flow for apietusam faccum esequi berum. Hentias porerum ent omniend itatempoer porem uga. Luptati optaquisist quibus rem quam unt',
+      createdAt: new Date()
+    }
+  ];
+
+  // helper function to repeat the event propagation
+  changed(value, property) {
+    this.changes.emit(value);
+  }
+}
+
+describe('Application flows', () => {
+  let fixture: ComponentFixture<FlogoApplicationFlowsComponent>,
+    de:      DebugElement, el:      HTMLElement;
+  function compileComponent() {
+    return TestBed.compileComponents();
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot({
+        provide: TranslateLoader,
+        useFactory: (http: Http) => new TranslateStaticLoader(http, '/base/dist/public/assets/i18n', '.json'),
+        deps: [Http]
+      })],
+      declarations: [ FlogoApplicationFlowsComponent, ModalComponent, Container ] // declare the test component
+    });
+  });
+
+  it('Should render 4 flows', done => {
+    compileComponent()
+      .then(() => {
+        fixture = TestBed.createComponent(Container);
+        fixture.detectChanges();
+        de = fixture.debugElement.queryAll(By.css('.flows-container > .flow'));
+        expect(de.length).toEqual(4);
+        done();
+      });
+  });
+
+  it('Should display the flow title', done => {
+    compileComponent()
+      .then(() => {
+        fixture = TestBed.createComponent(Container);
+        fixture.detectChanges();
+        de = fixture.debugElement.query(By.css('.title:nth-of-type(1)'));
+        el = de.nativeElement;
+        expect(el.innerText).toEqual('Manually Adjust Temperature');
+        done();
+      });
+  });
+
+  it('Should display the flow description', done => {
+    compileComponent()
+      .then(() => {
+        fixture = TestBed.createComponent(Container);
+        fixture.detectChanges();
+        de = fixture.debugElement.queryAll(By.css('.description'));
+        el = de[1].nativeElement;
+        expect(el.innerText).toEqual('A basic flow for apietusam');
+        done();
+      });
+  });
+
+});
