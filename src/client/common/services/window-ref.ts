3c9e2cbfc45c80512ae6d8ae70c553719652525b
Fabian Castillo <fcastill@tibco.com>
2017-03-02 11:46:48 -0800

    Implement service to manage pop-out logs window.
    Part of #323, enables the creation and management of a secondary or child window.

---
+++ src/client/common/services/window-ref.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+function _window(): Window {
+  return window;
+}
+
+
+@Injectable()
+/**
+ * An injectable reference to the native window
+ */
+export class WindowRef {
+  get nativeWindow(): Window {
+    return _window();
+  }
+}
