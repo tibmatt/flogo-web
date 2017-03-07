4ac4bfe139e622db80fac3089846069416b3151a
Fabian Castillo <fcastill@tibco.com>
2017-03-07 15:04:38 -0800

    Hide nav bar for maximized log window

--- src/client/app/flogo/components/flogo.component.ts
+++ src/client/app/flogo/components/flogo.component.ts
@@ -1,35 +1,48 @@
-import {Component} from '@angular/core';
-import {Router, NavigationEnd, NavigationCancel} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params, NavigationEnd, NavigationCancel } from '@angular/router';
 
-import {LoadingStatusService} from '../../../common/services/loading-status.service';
+import { LoadingStatusService } from '../../../common/services/loading-status.service';
 import { LanguageService } from '../../../common/services/language.service';
 import { Observable } from 'rxjs/Observable';
+
+//todo:remove
+import 'rxjs/add/operator/do';
 
 
 @Component({
   selector: 'flogo-app',
   moduleId: module.id,
   templateUrl: 'flogo.tpl.html',
-  styleUrls: [ 'flogo.component.css' ]
+  styleUrls: ['flogo.component.css']
 })
 
-export class FlogoAppComponent {
+export class FlogoAppComponent implements OnInit {
 
-  public isPageLoading : Observable<boolean>;
+  public isPageLoading: Observable<boolean>;
+  public showNav: boolean = true;
 
-  constructor(public router : Router,
-              public loadingStatusService : LoadingStatusService,
-              public languageService: LanguageService){
+  constructor(public router: Router,
+              public loadingStatusService: LoadingStatusService,
+              public languageService: LanguageService,
+              private activatedRoute: ActivatedRoute,
+  ) {
 
     this.isPageLoading = this.loadingStatusService.status;
+    this.languageService.configureLanguage();
+  }
 
-    this.router.events.subscribe((event:any):void => {
+  public ngOnInit() {
+
+    this.router.events.subscribe((event: any): void => {
       if (event instanceof NavigationEnd || event instanceof NavigationCancel) {
         this.loadingStatusService.stop();
       }
     });
 
-    this.languageService.configureLanguage();
+    this.activatedRoute.queryParams
+      .do(params => console.log('params', params))
+      .subscribe((params: Params) => this.showNav = !params['nonav']);
+
   }
 
 
