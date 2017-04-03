c3636e8bb5bbe7d54d2c6cd407fcd8e9d110ab36
Anand Pandurangi <apandura@tibco.com>
2017-04-03 23:42:17 +0530

    Changes to fix the travis build process and a small change to maintain consistent height of the application detail's list in the landing page even when there is no description.

--- src/client/app/flogo.apps/flogo.apps.routing.ts
+++ src/client/app/flogo.apps/flogo.apps.routing.ts
@@ -2,7 +2,6 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ConfigurationLoadedGuard } from '../../common/services/configuration-loaded-guard.service';
-import { FlogoAppsComponent } from './components/apps.component';
 import { FlogoMainComponent } from '../flogo.apps.main/components/main.component';
 import { FlogoApplicationContainerComponent } from '../flogo.apps.details/components/container.component';
 
