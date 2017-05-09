2213f39bdd8b51e5f13a795e5df9e6fa2988e6b6
Anand Pandurangi <apandura@tibco.com>
2017-05-09 12:54:52 +0530

    Fixes #310 - Removed the routing configuration for a old url(/flows) and the unused components

--- src/client/app/flogo/flogo.routing.ts
+++ src/client/app/flogo/flogo.routing.ts
@@ -1,22 +1,13 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FlogoFlowsComponent } from "../flogo.flows/components/flows.component";
 import { FlogoFlowsDetail as FlogoFlowsDetailComponent } from "../flogo.flows.detail/components/flow-detail.component";
 import { FlogoConfigComponent } from "../flogo.config/components/config.component";
 import { ConfigurationLoadedGuard } from '../../common/services/configuration-loaded-guard.service';
 
 
 export const appRoutes: Routes = [
-/*  {
-    path: 'flows',
-    canActivate: [ ConfigurationLoadedGuard ],
-    //name: "FlogoFlows",
-    loadChildren: '/app/flogo.flows/flogo.flows.module#FlowsModule'//,
-    //component: FlogoFlowsComponent,
-    //useAsDefault: true
-  },
-  {
+/* {
     path: 'flows/:id',
     canActivate: [ ConfigurationLoadedGuard ],
     //name:"FlogoFlowDetail",
@@ -29,7 +20,7 @@ export const appRoutes: Routes = [
     canActivate: [ ConfigurationLoadedGuard ]
   },*/
   {
-    path: '',
+    path: '**',
     redirectTo: 'apps',
     pathMatch: 'full'
   }
