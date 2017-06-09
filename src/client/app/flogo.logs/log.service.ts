faca8edc06d9bf3f9db5c47834065307e1f3f9a6
Fabian Castillo <fcastill@tibco.com>
2017-06-08 23:19:46 -0700

    Fix logs not working in client app

--- src/client/app/flogo.logs/log.service.ts
+++ src/client/app/flogo.logs/log.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { connect as ioConnect } from 'socket.io-client';
+import { environment } from '../../environments/environment';
 import * as io from 'socket.io-client';
 @Injectable()
 export class LogService {
@@ -8,7 +8,7 @@ export class LogService {
 
     constructor() {
         this.lines = [];
-        let socket = io();
+        let socket = io(environment.hostname);
         socket.on('on-connecting', this.onData.bind(this));
         socket.on('on-log', this.onData.bind(this));
     }
