03a484e5461e480be19b60ac90aef8e48d6dee03
Fabián Castillo <fcastill@tibco.com>
2017-08-25 19:42:28 -0700

    (cicd): Run UI unit tests as part of the build process. (#485)
    Added a “test” stage to run unit tests as part of the build process in travis.

--- src/client/karma.conf.js
+++ src/client/karma.conf.js
@@ -1,7 +1,18 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
+function getReporters(config) {
+  if (process.env.TRAVIS) {
+    return ['mocha'];
+  }
+  return config.angularCli && config.angularCli.codeCoverage
+    ? ['progress', 'coverage-istanbul']
+    : ['progress', 'kjhtml']
+}
+
 module.exports = function (config) {
+  const isTestEnv = process.env.FG_ENV === 'test';
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -10,7 +21,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-mocha-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -31,14 +43,27 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+    reporters: getReporters(config),
+    // reporter options
+    mochaReporter: {
+      // output: isTestEnv ? 'minimal' : 'full'
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222',
+        ]
+      }
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: process.env.TRAVIS ?  ['Chrome_travis_ci'] : ['Chrome'],
     singleRun: false
   });
 };
