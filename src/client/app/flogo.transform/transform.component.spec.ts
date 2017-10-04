831851385cfabf844ad08f634def5820bab5ea13
Fabian Castillo <fcastill@tibco.com>
2017-10-04 12:17:40 -0700

    Fix failing tests

--- src/client/app/flogo.transform/transform.component.spec.ts
+++ src/client/app/flogo.transform/transform.component.spec.ts
@@ -73,7 +73,7 @@ describe('Component: TransformComponent', () => {
   it('Should open the transform component', () => {
     fixture.detectChanges();
     expect(comp.isActive).toBeTruthy('Transform component is not active');
-    expect(de.query(By.css('.flogo-transform-modal'))).not.toBeNull('Transform modal is not present');
+    expect(de.query(By.css('.data-transform-modal'))).not.toBeNull('Transform modal is not present');
   });
 
   function getMockData() {
