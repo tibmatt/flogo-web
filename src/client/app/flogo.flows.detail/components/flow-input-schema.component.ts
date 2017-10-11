75596e58669a130381d3d1be2cc53a920582ab21
ylakkaku <ylakkaku@tibco.com>
2017-10-11 22:06:12 +0530

    Lowercasing and selecting default param type and UI changes

--- src/client/app/flogo.flows.detail/components/flow-input-schema.component.ts
+++ src/client/app/flogo.flows.detail/components/flow-input-schema.component.ts
@@ -1,9 +1,9 @@
 import {Component, Input, ViewChild} from '@angular/core';
 import {ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
 import {TranslateService} from 'ng2-translate/ng2-translate';
-import {FLOGO_TASK_ATTRIBUTE_TYPE} from "../../../common/constants";
-import {IFlowMetadataAttribute} from "../models/profiles/microservice-converter.model";
-import { FlogoFlowService as FlowsService } from '../services/flow.service';
+import {FLOGO_TASK_ATTRIBUTE_TYPE} from '../../../common/constants';
+import {IFlowMetadataAttribute} from '../models/profiles/microservice-converter.model';
+import {FlogoFlowService as FlowsService} from '../services/flow.service';
 
 
 @Component({
@@ -15,13 +15,13 @@ import { FlogoFlowService as FlowsService } from '../services/flow.service';
 export class FlogoFlowInputSchemaComponent {
   @ViewChild('modal')
   public modal: ModalComponent;
-  @Input()  flow: any;
+  @Input() flow: any;
   selectTypes: any[] = [];
   selectedParam: string;
   public definedInputParams: IFlowMetadataAttribute[] = [];
   public definedOutputParams: IFlowMetadataAttribute[] = [];
-  public otherInputItem: IFlowMetadataAttribute = {name: '', type: ''};
-  public otherOutputItem: IFlowMetadataAttribute = {name: '', type: ''};
+  public otherInputItem: IFlowMetadataAttribute = {name: '', type: 'string'};
+  public otherOutputItem: IFlowMetadataAttribute = {name: '', type: 'string'};
   displayInputParams: boolean;
 
 
@@ -29,36 +29,39 @@ export class FlogoFlowInputSchemaComponent {
               private _flowService: FlowsService,) {
 
   }
- getParams() {
-   let options = Object.keys(FLOGO_TASK_ATTRIBUTE_TYPE);
-   this.selectTypes = options.slice(options.length / 2);
-   this.definedInputParams = this.flow.metadata.input.map(input => ({
-     name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
-   }));
-   this.definedOutputParams = this.flow.metadata.output.map(input => ({
-     name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
-   }));
- }
+
+  getParams() {
+    const options = Object.keys(FLOGO_TASK_ATTRIBUTE_TYPE);
+    this.selectTypes = options.slice(options.length / 2);
+    this.definedInputParams = this.flow.metadata.input.map(input => ({
+      name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
+    }));
+    this.definedOutputParams = this.flow.metadata.output.map(input => ({
+      name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
+    }));
+  }
+
   showOutputParams() {
     this.displayInputParams = false;
     this.getParams();
   };
-  showInputParams(){
+
+  showInputParams() {
     this.displayInputParams = true;
   }
 
   public openInputSchemaModel() {
     this.displayInputParams = true;
-    this.otherInputItem = {name: '', type: ''};
+    this.otherInputItem = {name: '', type: 'string'};
     this.modal.open();
     this.getParams();
   }
 
   selectParamType(option: string) {
     this.selectedParam = option;
-    if (this.displayInputParams){
+    if (this.displayInputParams) {
       this.otherInputItem.type = this.selectedParam;
-    }else {
+    } else {
       this.otherOutputItem.type = this.selectedParam;
     }
   }
@@ -68,38 +71,36 @@ export class FlogoFlowInputSchemaComponent {
       return;
     }
     this.definedInputParams.push({name: this.otherInputItem.name, type: this.otherInputItem.type});
-    this.otherInputItem = {name: '', type: ''};
+    this.otherInputItem = {name: '', type: 'string'};
   }
   addOutputParams() {
     if (!this.otherOutputItem.name || !this.otherOutputItem.type) {
       return;
     }
     this.definedOutputParams.push({name: this.otherOutputItem.name, type: this.otherOutputItem.type});
-    this.otherOutputItem = {name: '', type: ''};
+    this.otherOutputItem = {name: '', type: 'string'};
   }
-   saveParams() {
-     const inputOutputParams = [];
-     console.log(this.flow);
-     delete this.flow.metadata;
-     this.addInputParams();
-     this.flow.metadata = {
-       'input': this.definedInputParams.map(input => ({
-         name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
-       })),
-       'output': this.definedOutputParams.map(input => ({
-         name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
-       }))
-     };
 
-     console.log(this.flow);
-     return this._flowService.saveFlow(this.flow.id, this.flow).then(rsp => {
-       console.groupCollapsed('Flow updated');
-       console.log(rsp);
-       console.groupEnd();
-       return rsp;
-     });
+  saveParams() {
+    delete this.flow.metadata;
+    this.addInputParams();
+    this.addOutputParams();
+    this.flow.metadata = {
+      'input': this.definedInputParams.map(input => ({
+        name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
+      })),
+      'output': this.definedOutputParams.map(input => ({
+        name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
+      }))
+    };
+    return this._flowService.saveFlow(this.flow.id, this.flow).then(rsp => {
+      console.groupCollapsed('Flow updated');
+      console.log(rsp);
+      console.groupEnd();
+      return rsp;
+    });
 
-   }
+  }
 
   removeParams(param) {
     if (this.displayInputParams) {
@@ -107,7 +108,7 @@ export class FlogoFlowInputSchemaComponent {
       if (inputIndex !== -1) {
         this.definedInputParams.splice(inputIndex, 1);
       }
-    }else {
+    } else {
       const outputIndex = this.definedOutputParams.indexOf(param);
       if (outputIndex !== -1) {
         this.definedOutputParams.splice(outputIndex, 1);
