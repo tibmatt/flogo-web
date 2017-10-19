5bf486de971c4da1e44e79ae8ef5324f33c077b6
Fabian Castillo <fcastill@tibco.com>
2017-10-18 18:00:44 -0700

    Flow schema: unique validator and update behavior

--- src/client/app/flogo.flows.detail/components/flow-input-schema.component.ts
+++ src/client/app/flogo.flows.detail/components/flow-input-schema.component.ts
@@ -1,9 +1,10 @@
 import {Component, Input, ViewChild, OnInit} from '@angular/core';
+import * as _ from 'lodash';
 import {ModalComponent} from 'ng2-bs3-modal/ng2-bs3-modal';
 import {TranslateService} from 'ng2-translate/ng2-translate';
 import {FLOGO_TASK_ATTRIBUTE_TYPE} from '../../../common/constants';
 import {FlogoFlowService as FlowsService} from '../services/flow.service';
-import {FormGroup, FormArray, FormBuilder, Validators} from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 
 @Component({
@@ -43,15 +44,6 @@ export class FlogoFlowInputSchemaComponent implements OnInit {
     this.displayInputParams = true;
   }
 
-  modifyParamType(event, index: number, fromParams: string) {
-    const data: any = {};
-    data.name = event.name;
-    data.type = FLOGO_TASK_ATTRIBUTE_TYPE[event.type];
-    const control = <FormArray>this.paramsForm.controls[fromParams];
-    const paramControl = this.createParamRow(data);
-    control.setControl(index, paramControl);
-  }
-
   openInputSchemaModel() {
     this.displayInputParams = true;
     this.initForm();
@@ -60,18 +52,22 @@ export class FlogoFlowInputSchemaComponent implements OnInit {
 
   addParams(fromParams: string) {
     const control = <FormArray>this.paramsForm.controls[fromParams];
-    const paramControl = this.createParamRow();
+    const paramControl = this.createParamFormRow();
     control.push(paramControl);
   }
 
-  saveParams(model: any) {
-    this.flow.metadata = model.value;
-    this.flow.metadata.input = this.flow.metadata.input.map(input => ({
-      name: input.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(input, 'type', 'STRING')],
-    }));
-    this.flow.metadata.output = this.flow.metadata.output.map(output => ({
-      name: output.name, type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(output, 'type', 'STRING')],
-    }));
+  saveParams() {
+    const mapParamsToFlow = (params) => params
+      // filter out empty attributes
+      .filter(param => param.name && param.name.trim().length > 0)
+      .map(param => ({
+        name: param.name.trim(),
+        type: FLOGO_TASK_ATTRIBUTE_TYPE[_.get(param, 'type', 'STRING')],
+      }));
+
+    const updatedParams = this.paramsForm.value;
+    this.flow.metadata.input = mapParamsToFlow(updatedParams.input);
+    this.flow.metadata.output = mapParamsToFlow(updatedParams.output);
 
     return this._flowService.saveFlow(this.flow.id, this.flow).then(rsp => {
       console.groupCollapsed('Flow updated');
@@ -92,18 +88,60 @@ export class FlogoFlowInputSchemaComponent implements OnInit {
     control.removeAt(index);
   }
 
-  private createParamRow(data?: { name: string, type: string }) {
+  private initForm() {
+    this.paramsForm = this._fb.group({
+      input: this.mapAttributesToFormArray(this.flow.metadata.input),
+      output: this.mapAttributesToFormArray(this.flow.metadata.output),
+    });
+  }
+
+  private mapAttributesToFormArray(attributes: { name: string, type: string}[]) {
+    const formArray = this._fb.array(
+      attributes.map(attribute => this.createParamFormRow(attribute)),
+      this.uniqueNameInGroupValidator,
+    );
+    // ensure default row
+    if (formArray.length === 0) {
+      formArray.push(this.createParamFormRow());
+    }
+    return formArray;
+  }
+
+  private createParamFormRow(data?: { name: string, type: string }) {
     return this._fb.group({
-      name: [data ? data.name : '', Validators.required],
+      name: [data ? data.name : ''],
       type: [data ? FLOGO_TASK_ATTRIBUTE_TYPE[data.type] : 'string'],
     });
   }
 
-  private initForm() {
-    this.paramsForm = this._fb.group({
-      input: this._fb.array(this.flow.metadata.input.map(input => this.createParamRow(input))),
-      output: this._fb.array(this.flow.metadata.output.map(output => this.createParamRow(output)))
-    });
+  private uniqueNameInGroupValidator(formArray: FormArray): {[key: string]: boolean} {
+    const nameControls = formArray.controls.map(group => group.get('name'));
+    const uniqueError = { uniqueName: true };
+    let formArrayHasErrors = false;
+    nameControls
+      .forEach((nameControl: AbstractControl) => {
+        const currentNameValue = nameControl.value && nameControl.value.trim();
+        let controlHasErrors = false;
+        if (currentNameValue) {
+          const repeatedControls = nameControls.filter(c => c !== nameControl && c.value && c.value.trim() === currentNameValue);
+          if (repeatedControls.length > 0) {
+            formArrayHasErrors = true;
+            controlHasErrors = true;
+            [nameControl, ...repeatedControls].forEach(control => {
+              control.setErrors(Object.assign({}, control.errors || {}, uniqueError));
+              // control.updateValueAndValidity({onlySelf: true});
+            });
+          }
+        }
+        if (!controlHasErrors) {
+          const newErrors = nameControl.errors;
+          if (newErrors) {
+            delete newErrors['uniqueName'];
+          }
+          nameControl.setErrors(!_.isEmpty(newErrors) ? newErrors : null);
+        }
+      });
+    return formArrayHasErrors ? uniqueError : null;
   }
 
 }
