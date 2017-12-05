3c42ed0829f0f63eebba83cd1c16ab34a15472b7
Anand Pandurangi <apandura@tibco.com>
2017-12-05 16:19:25 +0530

    Removed unused flogo.flows.import component

--- src/client/flogo/flogo.flows/flogo.flows.module.ts
+++ src/client/flogo/flogo.flows/flogo.flows.module.ts
@@ -6,7 +6,6 @@ import {Ng2Bs3ModalModule} from 'ng2-bs3-modal/ng2-bs3-modal';
 
 import { SharedModule as FlogoSharedModule } from '@flogo/shared';
 import { FlogoNewFlowComponent as FlogoFlowsAddComponent } from '../app/new-flow/new-flow.component';
-import { FlogoFlowsImportComponent as FlogoFlowsImportComponent } from '../flogo.flows.import/components/import-flow.component';
 import { FlogoFlowsFlowNameComponent } from '../flogo.flows.flow-name/components/flow-name.component';
 
 
@@ -19,12 +18,10 @@ import { FlogoFlowsFlowNameComponent } from '../flogo.flows.flow-name/components
   ],
   declarations: [
     FlogoFlowsAddComponent,
-    FlogoFlowsImportComponent,
     FlogoFlowsFlowNameComponent
   ],
   exports: [
     FlogoFlowsAddComponent,
-    FlogoFlowsImportComponent,
     FlogoFlowsFlowNameComponent
   ]
 })
