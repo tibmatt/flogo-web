cbd1acfe838265d20f56e1773fd6ce40ac657f80
Fabian Castillo <fcastill@tibco.com>
2017-12-12 14:10:50 -0800

    Clean all dev files on dev start

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -9,10 +9,10 @@ import del from 'del';
 gulp.task('clean', 'Delete application files from client and server dist folders to have a clean state for build process', ['clean.client', 'clean.server']);
 
 gulp.task('clean.client', 'Delete application files from client dist folder', () => {
-  return del.sync(['**/*', '!node_modules/**', '!web.log', '!engine.log'], {cwd: CONFIG.paths.dist.public});
+  return del.sync(['**/*', '!node_modules/**', '!web.log', '!engine.log'], { cwd: CONFIG.paths.dist.public, force: true });
 });
 
 gulp.task('clean.server', 'Delete application files from server dist folder', () => {
-  return del.sync(['**/*', '!local/**', '!node_modules/**', '!docker-shared/**', '!log.txt'], {cwd: CONFIG.paths.dist.server});
+  return del.sync(['**/*', '!local/**', '!node_modules/**', '!docker-shared/**', '!log.txt'], { cwd: CONFIG.paths.dist.server, force: true });
 });
 
