4f431da1697b1de0054ec0f0af0fceb52642cda6
Fabian Castillo <fcastill@tibco.com>
2018-02-20 21:35:00 -0800

    JSON Schemas for new app model

---
+++ src/server/modules/schemas/v1.0.0/index.js
@@ -0,0 +1,8 @@
+export { SCHEMA_NAMESPACE } from './schema-namespace';
+
+const common = require('./common');
+const trigger = require('./trigger');
+const flow = require('./flow');
+const app = require('./app');
+
+export { common, trigger, flow, app };
---
+++ src/server/modules/schemas/v1.0.0/schema-namespace.js
@@ -0,0 +1,1 @@
+export const SCHEMA_NAMESPACE = 'http://github.com/TIBCOSoftware/flogo/schemas/1.0.0';
