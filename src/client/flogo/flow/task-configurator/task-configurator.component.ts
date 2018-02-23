72455dc16bdf78acff333324a53c00365bac4838
ylakkaku <ylakkaku@tibco.com>
2018-02-23 12:35:21 +0530

    Changes to input mappings when metadata is not found for a subflow.

--- src/client/flogo/flow/task-configurator/task-configurator.component.ts
+++ src/client/flogo/flow/task-configurator/task-configurator.component.ts
@@ -224,8 +224,8 @@ export class TaskConfiguratorComponent implements OnDestroy {
     let mappings = [];
     if (data.overridePropsToMap) {
       propsToMap = data.overridePropsToMap;
-    } else if (this.isSubflowType && data.subflowSchema.metadata) {
-      propsToMap = data.subflowSchema.metadata.input;
+    } else if (this.isSubflowType) {
+      propsToMap = data.subflowSchema.metadata ? data.subflowSchema.metadata.input : [];
     } else if (this.currentTile.attributes && this.currentTile.attributes.inputs) {
       propsToMap = this.currentTile.attributes.inputs;
     }
