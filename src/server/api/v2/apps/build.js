53c0e913815b9e3759392bcf1925ae1de4f0e52b
Fabian Castillo <fcastill@tibco.com>
2018-03-08 14:14:24 -0800

    Add options in UI to compile to windows/amd64 and linux/amd64.

---
+++ src/server/api/v2/apps/build.js
@@ -0,0 +1,28 @@
+import kebabCase from 'lodash/kebabCase';
+import { AppsManager } from '../../../modules/apps/index.v2';
+
+export function* buildApp() {
+  const appId = this.params.appId;
+  const options = { compile: {} };
+
+  options.compile.os = this.query.os || null;
+  options.compile.arch = this.query.arch || null;
+
+  const result = yield AppsManager.build(appId, options);
+  const name = getNameForFile(result.appName, options.compile);
+  this.attachment(name);
+  this.body = result.data;
+}
+
+function getNameForFile(appName, { os: targetOs, arch: targetArch }) {
+  let name = [
+    kebabCase(appName),
+    targetOs || undefined,
+    targetArch || undefined,
+  ].join('_');
+  if (targetOs === 'windows') {
+    name = `${name}.exe`;
+  }
+  return name;
+}
+
