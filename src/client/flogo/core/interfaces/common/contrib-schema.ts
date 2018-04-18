41bf86f8e77c4efee3f8c65a4033b5e2df6216df
Fabian Castillo <fcastill@tibco.com>
2018-04-18 16:31:52 -0700

    Add contrib schemas

---
+++ src/client/flogo/core/interfaces/common/contrib-schema.ts
@@ -0,0 +1,49 @@
+import { ValueType } from '@flogo/core/constants';
+
+export type ContribSchema = ActivitySchema | TriggerSchema;
+
+export interface ActivitySchema extends BaseSchema {
+  type: 'flogo:activity';
+  return: boolean;
+  /** @deprecated */
+  reply?: boolean;
+  input: SchemaAttribute[];
+}
+
+export interface TriggerSchema extends BaseSchema {
+  type: 'flogo:trigger';
+  reply?: SchemaAttribute[];
+  settings?: SchemaAttribute[];
+  handler?: {
+    settings: SchemaAttribute[];
+  };
+}
+
+interface SchemaAttribute {
+  name: string;
+  type: ValueType;
+  required?: boolean;
+  allowed?: any[];
+  // default value
+  value?: any;
+  display?: SchemaDisplay;
+}
+
+interface SchemaDisplay {
+  name: string;
+  type: string;
+  mapperOutputScope: string;
+}
+
+export interface BaseSchema {
+  name: string;
+  type: 'flogo:trigger' | 'flogo:activity';
+  ref: string;
+  version: string;
+  title: string;
+  description: string;
+  author?: string;
+  homepage: string;
+  output?: SchemaAttribute[];
+}
+
