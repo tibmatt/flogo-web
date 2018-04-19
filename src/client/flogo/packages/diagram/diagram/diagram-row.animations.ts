19ca034aac7a1a0e60aa8c8aeb52dc83ec211f99
Fabian Castillo <fcastill@tibco.com>
2018-04-19 13:59:03 -0700

    Fix animation when removing a tile placed before another tile with branches.

--- src/client/flogo/packages/diagram/diagram/diagram-row.animations.ts
+++ src/client/flogo/packages/diagram/diagram/diagram-row.animations.ts
@@ -1,7 +1,19 @@
 import { animate, group, keyframes, style, transition, trigger } from '@angular/animations';
 
+const shrink = animate('0.3s ease-out', keyframes([
+    style({ width: '*', offset: 0 }),
+    style({ width: '*', offset: 0.6 }),
+    style({ width: 0, offset: 1 }),
+  ]));
+
 export const rowAnimations = [
-  trigger('tile-task', [
+  trigger('tileGeneric', [
+    transition(':leave', [
+      style({ transform: 'scale(1)', opacity: 1, width: '*' }),
+      shrink,
+    ])
+  ]),
+  trigger('tileTask', [
     transition(':enter', [
       style({ transform: 'scale(0.7)', opacity: 0 }),
       animate('0.3s cubic-bezier(.8,-0.6,0.2,1.5)',
@@ -12,31 +24,15 @@ export const rowAnimations = [
       group([
         animate('0.15s ease-in',
           style({ transform: 'scale(0.7)', opacity: 0 })),
-        animate('0.3s ease-out', keyframes([
-          style({ width: '*', offset: 0 }),
-          style({ width: '*', offset: 0.6 }),
-          style({ width: 0, offset: 1 }),
-        ])),
+        shrink,
       ]),
     ])
   ]),
-  trigger('tile-insert', [
+  trigger('tileInsert', [
     transition(':leave', [
       style({ opacity: 1, position: 'relative', zIndex: '-1' }),
       animate('0s cubic-bezier(.8,-0.6,0.2,1.5)',
         style({ opacity: 0 }))
     ])
-  ]),
-  trigger('tile-branch', [
-    // transition(':enter', [
-    //   style({ transform: 'translateY(-50%) scale(0.7)', opacity: 0 }),
-    //   animate('0.3s cubic-bezier(.8,-0.6,0.2,1.5)',
-    //     style({ transform: 'translateY(0) scale(1)', opacity: 1 }))
-    // ]),
-    // transition(':leave', [
-    //   style({ transform: 'scale(1)', opacity: 1 }),
-    //   animate('0.3s cubic-bezier(.8,-0.6,0.2,1.5)',
-    //     style({ transform: 'scale(0.7)', opacity: 0 }))
-    // ])
   ]),
 ];
