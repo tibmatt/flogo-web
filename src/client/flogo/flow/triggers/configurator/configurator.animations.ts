4ef8e463b5d9a1680ba501825875617b75fa797e
Fabian Castillo <fcastill@tibco.com>
2018-05-07 12:02:28 -0700

    Adjust animation timings

--- src/client/flogo/flow/triggers/configurator/configurator.animations.ts
+++ src/client/flogo/flow/triggers/configurator/configurator.animations.ts
@@ -1,13 +1,4 @@
-import { animate, group, keyframes, query, sequence, stagger, state, style, transition, trigger } from '@angular/animations';
-
-const FLOW_HEADER_HEIGHT = 167;
-const FLOW_TRIGGER_VERTICAL_PADDING = 40;
-const FLOW_TRIGGER_HORIZONTAL_PADDING = 22;
-const CONFIGURATOR_HEADER_HEIGHT = 103;
-const CONFIGURATOR_PADDING = 40;
-const CONFIGURATOR_TRIGGER_PANEL_WIDTH = 230;
-
-const toPixels = number => `${number}px`;
+import { animate, animateChild, group, keyframes, query, sequence, state, style, transition, trigger } from '@angular/animations';
 
 export const configuratorAnimations = [
   trigger('configurationPanel', [
@@ -16,7 +7,7 @@ export const configuratorAnimations = [
         query('.background', [
           style({ width: '88px', transform: 'translateY(167px)', opacity: 0 }),
           group([
-            animate('200ms ease-out', keyframes([
+            animate('100ms ease-out', keyframes([
               style({ opacity: 0.5, offset: 0.3 }),
               style({ opacity: 1, offset: 1 }),
             ])),
@@ -28,41 +19,31 @@ export const configuratorAnimations = [
             })),
           ]),
         ]),
-        query('.js-triggers-list', [
-          style({
-            transform: 'translateY(104px)',
-          }),
-          animate('300ms 50ms cubic-bezier(0.55, 0.085, 0.68, 0.53)', style('*'))
-        ]),
         query('.js-trigger-element', [
           style({
-            // transform: 'translate(-18px, 103px)',
-            transform: 'translateX(-18px)',
             width: '46px',
             marginBottom: '11px',
             opacity: 0,
+            transform: 'translate(-18px, 104px)',
           }),
           group([
-            animate('300ms 50ms linear', style({
-              transform: 'translateX(0)',
-            })),
             sequence([
               animate('50ms ease', style({ opacity: 1 })),
-              animate('300ms 100ms ease-out', style({
-                width: '*',
-                marginBottom: '*',
-              })),
+              group([
+                animate('100ms ease', style({ marginBottom: '*' })),
+                animate('250ms ease-in-out', style({ width: '*' })),
+                animate('250ms cubic-bezier(0.4, 0.0, 0.2, 1)', style({ transform: 'translate(0)' })),
+              ]),
             ])
           ]),
         ]),
-        query('.js-animation-content', [
-          style({ opacity: 0, transform: 'translateX(-10px)' }),
-          animate('300ms 500ms cubic-bezier(0.0, 0.0, 0.2, 1)', style('*')),
+        query('@triggerConfiguratorLayoutElement', [
+          animateChild(),
         ]),
       ]),
     ]),
     transition('* => void', [
-      query('.js-animation-content', [
+      query('@triggerConfiguratorLayoutElement', [
         animate('80ms ease-out', style({ opacity: 0, transform: 'translateY(-10px)' })),
       ]),
       group([
@@ -89,5 +70,14 @@ export const configuratorAnimations = [
         ]),
       ])
     ])
-  ])
+  ]),
+  trigger('triggerConfiguratorLayoutElement', [
+    state('*', style({ opacity: 1 })),
+    transition('void => *', [
+      style({ opacity: '*' }),
+      animate('300ms 200ms cubic-bezier(0.0, 0.0, 0.2, 1)', style({
+        opacity: 1,
+      })),
+    ])
+  ]),
 ];
