f5867d8b838d8bee57a8424bdd8da38405c73cd6
Anand Pandurangi <apandura@tibco.com>
2018-07-28 02:16:37 +0530

    small alignment changes and also fixed the issue where the activity is listed after successful installation

--- src/client/flogo/flow/task-add-new/core/filter-activities-by.ts
+++ src/client/flogo/flow/task-add-new/core/filter-activities-by.ts
@@ -3,16 +3,19 @@ import {map, shareReplay, withLatestFrom} from 'rxjs/operators';
 import {isEmpty, filter} from 'lodash';
 import {Activity} from '../task-add.component';
 
+const filterList = (activities, filterText) => {
+  if (filterText && !isEmpty(filterText)) {
+    return filter(activities, (activity: Activity) => activity.title.toLowerCase().indexOf(filterText.toLowerCase()) >= 0);
+  } else {
+    return activities;
+  }
+};
+
 export function filterActivitiesBy(sourceList$: Observable<Activity[]>, filterText$: ReplaySubject<string>): Observable<Activity[]> {
+  const mapTo = map(([searchTerm, activityList]) => filterList(activityList, searchTerm));
   return filterText$.pipe(
     shareReplay(),
     withLatestFrom(sourceList$),
-    map(([filterText, activities]) => {
-      if (filterText && !isEmpty(filterText)) {
-        return filter(activities, (activity: Activity) => activity.title.toLowerCase().indexOf(filterText.toLowerCase()) >= 0);
-      } else {
-        return activities;
-      }
-    })
+    mapTo
   );
 }
