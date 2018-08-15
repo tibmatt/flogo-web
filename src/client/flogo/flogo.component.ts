9bdf729505c5d845a75a12966ba1be8e440bd679
Fabian Castillo <fcastill@tibco.com>
2018-08-14 19:31:56 -0700

    Fix content changed after view checked error caused by loader animation

--- src/client/flogo/flogo.component.ts
+++ src/client/flogo/flogo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params, NavigationEnd, NavigationCancel, NavigationStart, RouterEvent } from '@angular/router';
+import { delay } from 'rxjs/operators';
 
 @Component({
   // disabling component-selector rule as the main application component name does not require it
@@ -18,13 +19,15 @@ export class FlogoAppComponent implements OnInit {
   }
 
   public ngOnInit() {
-    this.router.events.subscribe((event: RouterEvent): void => {
-      if (event instanceof NavigationStart) {
-        this.isPageLoading = true;
-      } else if (event instanceof NavigationEnd || event instanceof NavigationCancel) {
-        this.isPageLoading = false;
-      }
-    });
+    this.router.events
+      .pipe(delay(0))
+      .subscribe((event: RouterEvent): void => {
+        if (event instanceof NavigationStart) {
+          this.isPageLoading = true;
+        } else if (event instanceof NavigationEnd || event instanceof NavigationCancel) {
+          this.isPageLoading = false;
+        }
+      });
 
     this.activatedRoute.queryParams
       .subscribe((params: Params) => this.showNav = !params['nonav']);
