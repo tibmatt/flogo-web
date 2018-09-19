f2c2b9736f51d5c68e3b2fb7d8e2ca010583f9a1
Fabian Castillo <fcastill@tibco.com>
2018-09-19 11:12:53 -0700

    Copy files required by image but generated during docker build

---
+++ tools/release/copy-docker-files.js
@@ -0,0 +1,15 @@
+require('../utils/crash-on-unhandled-rejection');
+import { resolve } from 'path';
+import copy from 'cpy';
+import { Dist } from './config';
+
+// this files are not required by the web UI itself but by the docker image
+// files are copied into the root of the app during docker image build and they're expected to
+// be copied to dist folder
+copy(
+  ['.dockerignore', 'docker-start.dist.sh', 'Dockerfile.dist'],
+  resolve(Dist.root, '..'),
+  {
+    rename: basename => basename.replace('.dist', ''),
+  }
+);
