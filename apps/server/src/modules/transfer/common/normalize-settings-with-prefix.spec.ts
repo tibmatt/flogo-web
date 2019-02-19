4b78b84587b082023e8cf70e357a0394e636fa38
Anand Pandurangi <apandura@tibco.com>
2019-02-19 23:15:55 +0530

    Normalized the trigger and handler settings while importing

---
+++ apps/server/src/modules/transfer/common/normalize-settings-with-prefix.spec.ts
@@ -0,0 +1,18 @@
+import { normalizeSettingsWithPrefix } from './normalize-settings-with-prefix';
+
+describe('transfer.common.normalizeSettingsWithPrefix', () => {
+  it('Should normalize the setting value with the latest syntax', () => {
+    const sampleJSON = {'test': 'json'};
+    expect(normalizeSettingsWithPrefix({
+      'setting1': 'literal_value',
+      'setting2': '$assignment_value',
+      'setting3': 20,
+      'setting4': sampleJSON
+    })).toEqual({
+      'setting1': 'literal_value',
+      'setting2': '=$assignment_value',
+      'setting3': 20,
+      'setting4': '=' + JSON.stringify(sampleJSON, null, 2)
+    })
+  });
+});
---
+++ apps/server/src/modules/transfer/common/normalize-settings-with-prefix.ts
@@ -0,0 +1,19 @@
+import { isString, isPlainObject, mapValues } from 'lodash';
+import { EXPR_PREFIX, FlogoAppModel } from '@flogo-web/core';
+
+export function normalizeSettingsWithPrefix(settings: FlogoAppModel.Settings): FlogoAppModel.Settings {
+  return mapValues(settings, normalizeSetting);
+}
+
+function normalizeSetting(setting: any) {
+  if (isString(setting) && setting.startsWith('=')) {
+    return setting;
+  }
+  let normalizedValue = setting;
+  if (isPlainObject(setting)) {
+    normalizedValue = EXPR_PREFIX + JSON.stringify(setting, null, 2);
+  } else if (isString(setting) && setting.startsWith('$')) {
+    normalizedValue = EXPR_PREFIX + setting;
+  }
+  return normalizedValue;
+}
