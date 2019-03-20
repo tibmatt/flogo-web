c18c6e1f3f01c8b3942ebd053da4980ce009adc0
Fabian Castillo <fcastill@tibco.com>
2019-03-20 16:45:17 -0700

    clean: remove unused client components

--- apps/client/src/flogo/core/navbar/navbar.component.ts
+++ apps/client/src/flogo/core/navbar/navbar.component.ts
@@ -1,45 +1,17 @@
-import { isEmpty } from 'lodash';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { FLOGO_VERSION } from '@flogo-web/client-core';
 
 @Component({
   selector: 'flogo-navbar',
-  // moduleId: module.id,
   templateUrl: 'navbar.tpl.html',
   styleUrls: ['navbar.component.less'],
 })
-export class FlogoNavbarComponent implements OnInit {
+export class FlogoNavbarComponent {
   public currentYear: number;
-  public isWalkthroughActivated = false;
   isOpenMenu = false;
 
   constructor(@Inject(FLOGO_VERSION) public version: string) {
     this.currentYear = new Date().getFullYear();
-  }
-
-  ngOnInit() {
-    // TODO: re-enable after v0.5.1 with updated content
-    // setTimeout(() => {
-    //   this.showInstructions();
-    // }, 500);
-  }
-
-  showInstructions() {
-    // TODO: show again if walkthrough content was updated?
-    const instructions: string = localStorage.getItem('flogo-show-instructions');
-    if (isEmpty(instructions)) {
-      localStorage.setItem('flogo-show-instructions', new Date().toString());
-      this.isWalkthroughActivated = true;
-    }
-    return this.isWalkthroughActivated;
-  }
-
-  public onClosedInstructions(closed) {
-    this.isWalkthroughActivated = false;
-  }
-
-  public activateInstructions(event: any) {
-    this.isWalkthroughActivated = true;
   }
 
   toggleNavMenu() {
