5f5e04f85876296ab4217babe6c51ca2b5d34a51
Anand Pandurangi <apandura@tibco.com>
2019-03-29 21:13:55 +0530

    fix(server): Creating the application now maintains new appModel 1.1.0

--- apps/server/src/core/models/app.ts
+++ apps/server/src/core/models/app.ts
@@ -1,4 +1,5 @@
 import { App } from '@flogo-web/core';
+import { APP_MODEL_VERSION } from '../../common/constants';
 
 export function constructApp(inputData, generateId?: () => string): App {
   const now = new Date().toISOString();
@@ -8,6 +9,7 @@ export function constructApp(inputData, generateId?: () => string): App {
     name: inputData.name.trim(),
     createdAt: now,
     updatedAt: null,
+    appModel: APP_MODEL_VERSION,
     triggers: [],
     resources: [],
     imports: inputData.imports || [],
