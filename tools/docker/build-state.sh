99d3d22bf553204d44cb68efc42473a77d543b2a
Tracy Li <tibcotracy@gmail.com>
2019-04-09 16:25:16 -0500

    Build flogo-web image

---
+++ tools/docker/build-state.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+mkdir -p ${GOPATH}/src/github.com/TIBCOSoftware/
+
+#Hardcode to v0.5.8
+FLOGO_TAG_NAME=v0.5.8
+cd ${GOPATH}/src/github.com/TIBCOSoftware/
+	git clone https://github.com/TIBCOSoftware/flogo-services --branch ${FLOGO_TAG_NAME} --single-branch
+	git clone https://github.com/TIBCOSoftware/flogo-lib --branch ${FLOGO_TAG_NAME} --single-branch
+
+  go get ./...
+	#Build flogo state
+	pushd flogo-services/flow-store
+		go install ./...
+				ls -ls ${GOPATH}/bin/
+
+	popd
+	#Build flogo flow
+	pushd flogo-services/flow-state
+		go install ./...
+		ls -ls
+	popd
+
+
+cp ${GOPATH}/bin/* ${BUILD_DIR}/
+
+
---
+++ tools/docker/flogo-eula/server.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var http = require('http');
+var PORT = process.env.PORT || 3303;
+
+var server = http.createServer(function (req, res) {
+  var fileStream = fs.createReadStream('index.html');
+  fileStream.pipe(res);
+});
+server.listen(PORT, '0.0.0.0');  // start
+
