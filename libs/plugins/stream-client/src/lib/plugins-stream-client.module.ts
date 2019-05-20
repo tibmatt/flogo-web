bc535ee07eec3f25a4b940e0d8a4aa2210629a42
Fabian Castillo <fcastill@tibco.com>
2019-05-20 10:38:06 -0700

    feat(stream-client): display diagram

--- libs/plugins/stream-client/src/lib/plugins-stream-client.module.ts
+++ libs/plugins/stream-client/src/lib/plugins-stream-client.module.ts
@@ -2,17 +2,19 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { SharedModule as FlogoSharedModule } from '@flogo-web/lib-client/common';
 import { ContextPanelModule } from '@flogo-web/lib-client/context-panel';
+import { DiagramModule } from '@flogo-web/lib-client/diagram';
 
 import { StreamDesignerComponent } from './stream-designer/stream-designer.component';
 import { SimulatorComponent } from './simulator/simulator.component';
-import { SharedModule as FlogoSharedModule } from '@flogo-web/lib-client/common';
 
 @NgModule({
   imports: [
     CommonModule,
     FlogoSharedModule,
     ContextPanelModule,
+    DiagramModule,
     RouterModule.forChild([
       { path: '', pathMatch: 'full', component: StreamDesignerComponent },
     ]),
--- libs/plugins/stream-client/src/lib/stream-designer/stream-designer.component.ts
+++ libs/plugins/stream-client/src/lib/stream-designer/stream-designer.component.ts
@@ -2,6 +2,13 @@ import { Observable } from 'rxjs';
 import { scan } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { ValueType } from '@flogo-web/core';
+import {
+  DiagramSelection,
+  DiagramAction,
+  DiagramActionType,
+  DiagramActionSelf,
+  DiagramSelectionType,
+} from '@flogo-web/lib-client/diagram';
 
 import { SimulatorService } from '../simulator.service';
 
@@ -18,8 +25,13 @@ export class StreamDesignerComponent implements OnInit {
   backToAppHover = false;
   testingData = 'Anand';
   testingData1 = 'Some description';
+  graph;
+  currentSelection: DiagramSelection;
 
-  constructor(private simulationService: SimulatorService) {}
+  constructor(private simulationService: SimulatorService) {
+    const { mainGraph, mainItems } = mockResource();
+    this.graph = mainGraph;
+  }
 
   ngOnInit() {
     this.simulationData$ = this.simulationService.data$.pipe(
@@ -40,6 +52,16 @@ export class StreamDesignerComponent implements OnInit {
       ]);
     } else {
       this.simulationService.stopSimulation();
+    }
+  }
+
+  onDiagramAction(action: DiagramAction) {
+    if (action.type === DiagramActionType.Select) {
+      this.currentSelection = {
+        diagramId: 'stream',
+        taskId: (action as DiagramActionSelf).id,
+        type: DiagramSelectionType.Node,
+      };
     }
   }
 
@@ -64,4 +86,128 @@ export class StreamDesignerComponent implements OnInit {
   onMouseOutBackControl() {
     this.backToAppHover = false;
   }
+}
+
+function mockResource() {
+  return {
+    mainItems: {
+      log_3: {
+        name: 'Log',
+        description: 'Logs a message',
+        settings: {},
+        ref: 'github.com/project-flogo/contrib/activity/log',
+        id: 'log_3',
+        inputMappings: {
+          message: '',
+          addDetails: false,
+        },
+        type: 1,
+        return: false,
+        activitySettings: {},
+        input: {
+          message: '',
+          addDetails: false,
+        },
+      },
+      filter_4: {
+        name: 'Filter',
+        description: 'Simple Filter Activity',
+        settings: {},
+        ref: 'github.com/project-flogo/stream/activity/filter',
+        id: 'filter_4',
+        inputMappings: {},
+        type: 1,
+        return: false,
+        activitySettings: {},
+        input: {
+          value: null,
+        },
+      },
+      aggregate_5: {
+        name: 'Aggregate',
+        description: 'Simple Aggregate Activity',
+        settings: {},
+        ref: 'github.com/project-flogo/stream/activity/aggregate',
+        id: 'aggregate_5',
+        inputMappings: {},
+        type: 1,
+        return: false,
+        activitySettings: {},
+        input: {
+          value: null,
+        },
+      },
+    },
+    mainGraph: {
+      rootId: 'log_3',
+      nodes: {
+        log_3: {
+          type: 'task',
+          id: 'log_3',
+          title: 'Log',
+          description: 'Logs a message',
+          features: {
+            selectable: true,
+            canHaveChildren: true,
+            canBranch: false,
+            deletable: true,
+            subflow: false,
+            final: false,
+          },
+          status: {
+            invalid: false,
+            executed: false,
+            executionErrored: null,
+            iterable: false,
+          },
+          children: ['filter_4'],
+          parents: [],
+        },
+        filter_4: {
+          type: 'task',
+          id: 'filter_4',
+          title: 'Filter',
+          description: 'Simple Filter Activity',
+          features: {
+            selectable: true,
+            canHaveChildren: true,
+            canBranch: false,
+            deletable: true,
+            subflow: false,
+            final: false,
+          },
+          status: {
+            invalid: false,
+            executed: false,
+            executionErrored: null,
+            iterable: false,
+          },
+          children: ['aggregate_5'],
+          parents: ['log_3'],
+        },
+        aggregate_5: {
+          type: 'task',
+          id: 'aggregate_5',
+          title: 'Aggregate',
+          description: 'Simple Aggregate Activity',
+          features: {
+            selectable: true,
+            canHaveChildren: true,
+            canBranch: false,
+            deletable: true,
+            subflow: false,
+            final: false,
+          },
+          status: {
+            invalid: false,
+            executed: false,
+            executionErrored: null,
+            iterable: false,
+          },
+          children: [],
+          parents: ['filter_4'],
+        },
+      },
+    },
+  };
 }
