9cac96b77b2fa22617739239539ca22e33aedcc6
ppaidi <ppaidi@tibco.com>
2019-05-30 22:47:07 +0530

    feat(stream-client): implemented new diagram interaction, having a bar separator between tiles

--- libs/lib-client/diagram/src/lib/diagram.module.ts
+++ libs/lib-client/diagram/src/lib/diagram.module.ts
@@ -8,14 +8,20 @@ import { TileInsertComponent } from './tiles/tile-insert.component';
 import { TileTaskComponent } from './tiles/tile-task.component';
 
 import { TileBranchComponent } from './tiles/tile-branch.component';
+import { StreamDiagramComponent } from './diagram/stream-diagram.component';
+import { StreamDiagramRowComponent } from './diagram/stream-diagram-row.component';
+import { StreamTileTaskComponent } from './tiles/stream-tile-task.component';
 
 @NgModule({
   imports: [CommonModule, LanguageModule],
-  exports: [DiagramComponent],
+  exports: [DiagramComponent, StreamDiagramComponent],
   declarations: [
     DiagramComponent,
+    StreamDiagramComponent,
     DiagramRowComponent,
+    StreamDiagramRowComponent,
     TileInsertComponent,
+    StreamTileTaskComponent,
     TileBranchComponent,
     TileTaskComponent,
   ],
---
+++ libs/lib-client/diagram/src/lib/tiles/stream-tile-task.component.ts
@@ -0,0 +1,72 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  OnChanges,
+} from '@angular/core';
+import { trigger } from '@angular/animations';
+
+import { SvgRefFixerService } from '@flogo-web/lib-client/core';
+
+import { AbstractTileTaskComponent } from './abstract-tile-task.component';
+import { OpenCloseMenuAnimation } from './tile.animations';
+
+@Component({
+  selector: 'flogo-diagram-stream-tile-task',
+  templateUrl: './stream-tile-task.component.html',
+  styleUrls: ['./stream-tile-task.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [trigger('menuOptions', OpenCloseMenuAnimation)],
+})
+export class StreamTileTaskComponent extends AbstractTileTaskComponent
+  implements OnChanges {
+  @HostBinding('class.--with-branches') hasBranch = false;
+
+  constructor(svgFixer: SvgRefFixerService) {
+    super(svgFixer);
+  }
+
+  ngOnChanges(changes) {
+    super.ngOnChanges(changes);
+    this.hasBranch = this.tile.hasBranch;
+  }
+
+  get bgFill() {
+    if (this.hasRun) {
+      return this.fixSvgRef('url(#flogo-diagram-tile__bg--has-run)');
+    } else if (this.isSelected) {
+      return '#4e7ead';
+    } else {
+      return this.fixSvgRef('url(#flogo-diagram-tile__bg)');
+    }
+  }
+
+  get shadow() {
+    if (this.isSelected) {
+      return this.fixSvgRef('url(#flogo-diagram-tile__shadow--active)');
+    } else {
+      return this.fixSvgRef('url(#flogo-diagram-tile__shadow)');
+    }
+  }
+
+  get isTerminal() {
+    if (this.tile.isTerminalInRow) {
+      return true;
+    }
+    const { task } = this.tile;
+    if (task) {
+      const { final: isFinal, canHaveChildren } = task.features;
+      return isFinal || !canHaveChildren;
+    }
+    return false;
+  }
+
+  get errorMsg() {
+    const status = this.tile.task.status;
+    if (status && status.executionErrored) {
+      const [error] = status.executionErrored;
+      return error;
+    }
+    return null;
+  }
+}
