05cf30820787b5e6f9a171e2bcd2764bdbe8cd33
Fabian Castillo <fcastill@tibco.com>
2019-06-11 20:39:38 -0700

    ci: use debian as base for docker image and build ml image

--- make-web.sh
+++ make-web.sh
@@ -17,9 +17,12 @@ readonly RELEASE_VERSION=${RELEASE_VERSION:-latest}
 readonly CLI_VERSION=${CLI_VERSION:-master}
 readonly CORE_VERSION=${CORE_VERSION:-master}
 
-export BUILD_ARGS="--build-arg CLI_VERSION=${CLI_VERSION} --build-arg CORE_VERSION=${CORE_VERSION}"
+export BUILD_ARGS="--build-arg CLI_VERSION=${CLI_VERSION} --build-arg CORE_VERSION=${CORE_VERSION} --build-arg RELEASE_VERSION=${RELEASE_VERSION}"
 
 echo "RELEASE_VERSION=$RELEASE_VERSION | CLI_VERSION=$CLI_VERSION | CORE_VERSION=$CORE_VERSION"
 
 docker build ${BUILD_ARGS} --force-rm=true --rm=true -t flogo/flogo-web:${RELEASE_VERSION} -f tools/docker/Dockerfile .
 
+if [ "$1" == "--build-ml" ]; then
+  docker build ${BUILD_ARGS} --force-rm=true --rm=true -t flogo/flogo-web:${RELEASE_VERSION}-ml -f tools/docker/ml.Dockerfile .
+fi
