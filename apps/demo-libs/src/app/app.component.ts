87c5dcfa9ad2caa2554289ee41ee76e4614f03b1
Fabian Castillo <fcastill@tibco.com>
2019-06-14 20:18:30 -0700

    feat: add app for demo of client libs and setup e2e tests

---
+++ apps/demo-libs/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  pages = [
+    { label: 'Home', path: ['/'] },
+    { label: 'Notifications', path: ['/notifications'] },
+    { label: 'Modals', path: ['/modals'] },
+  ];
+}
+
+@Component({
+  selector: 'demo-home',
+  template: '<h1>Welcome to Flogo Web Libs demo!</h1>',
+})
+export class HomeComponent {}
