34f1d311ae00b15205f51dc979fa3f6a64bae57a
Fabian Castillo <fcastill@tibco.com>
2019-06-18 21:03:43 -0700

    refactor(flow-client): extract install activity list into its own component and fix highlight issue
    Also fixes issue with subflow option always highlighted when activity clicked.

--- libs/plugins/flow-client/src/lib/task-add/task-add.component.ts
+++ libs/plugins/flow-client/src/lib/task-add/task-add.component.ts
@@ -5,26 +5,18 @@ import {
   OnInit,
   ElementRef,
   AfterViewInit,
-  ViewChildren,
-  QueryList,
   HostListener,
-  ViewChild,
-  OnDestroy,
 } from '@angular/core';
-import { FocusTrapFactory, ActiveDescendantKeyManager } from '@angular/cdk/a11y';
-import { UP_ARROW, DOWN_ARROW, ENTER } from '@angular/cdk/keycodes';
+import { FocusTrapFactory } from '@angular/cdk/a11y';
 import { Observable, ReplaySubject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 import { Resource, CONTRIB_REFS } from '@flogo-web/core';
-import { SingleEmissionSubject } from '@flogo-web/lib-client/core';
 import { FlogoInstallerComponent } from '@flogo-web/lib-client/contrib-installer';
 
 import { filterActivitiesBy } from './core/filter-activities-by';
 import { Activity, TaskAddOptions } from './core/task-add-options';
 import { ModalService } from '@flogo-web/lib-client/modal';
 import { delay } from 'rxjs/operators';
-import { ActivityComponent } from './activity/activity.component';
 
 export const TASKADD_OPTIONS = new InjectionToken<TaskAddOptions>('flogo-flow-task-add');
 
@@ -32,19 +24,11 @@ export const TASKADD_OPTIONS = new InjectionToken<TaskAddOptions>('flogo-flow-ta
   templateUrl: 'task-add.component.html',
   styleUrls: ['task-add.component.less'],
 })
-export class TaskAddComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChildren(ActivityComponent) optionListItems: QueryList<ActivityComponent>;
-  // second list because there's no way to get the component reference and the element ref at the same time
-  @ViewChildren(ActivityComponent, { read: ElementRef }) optionListItemsRefs: QueryList<
-    ElementRef
-  >;
-  @ViewChild('activityList', { read: ElementRef }) activityListRef: ElementRef;
+export class TaskAddComponent implements OnInit, AfterViewInit {
   filteredActivities$: Observable<Activity[]>;
   filterText$: ReplaySubject<string>;
   isInstallOpen = false;
   isSubflowOpen = false;
-  private destroy$ = SingleEmissionSubject.create();
-  private keyboardEventsManager: ActiveDescendantKeyManager<ActivityComponent>;
 
   constructor(
     @Inject(TASKADD_OPTIONS) public control: TaskAddOptions,
@@ -58,13 +42,6 @@ export class TaskAddComponent implements OnInit, AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     const focusTrap = this.focusTrap.create(this.elementRef.nativeElement);
     focusTrap.focusInitialElement();
-    this.keyboardEventsManager = new ActiveDescendantKeyManager(this.optionListItems);
-    this.keyboardEventsManager.change.pipe(takeUntil(this.destroy$)).subscribe(() => {
-      this.ensureCurrentListOptionVisible();
-    });
-    this.filteredActivities$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => this.keyboardEventsManager.setActiveItem(null));
   }
 
   ngOnInit() {
@@ -73,10 +50,6 @@ export class TaskAddComponent implements OnInit, AfterViewInit, OnDestroy {
       this.filterText$
     );
     this.filterText$.next('');
-  }
-
-  ngOnDestroy() {
-    this.destroy$.emitAndComplete();
   }
 
   filterActivities(term: string) {
@@ -112,22 +85,6 @@ export class TaskAddComponent implements OnInit, AfterViewInit, OnDestroy {
     this.setSubflowWindowState(false);
   }
 
-  onFocusList() {
-    if (!this.keyboardEventsManager.activeItem) {
-      this.keyboardEventsManager.setFirstItemActive();
-    }
-  }
-
-  listKeyDown(event) {
-    if (event.keyCode === UP_ARROW || event.keyCode === DOWN_ARROW) {
-      this.keyboardEventsManager.onKeydown(event);
-      event.preventDefault();
-    } else if (event.keyCode === ENTER && this.keyboardEventsManager.activeItem) {
-      this.keyboardEventsManager.activeItem.select();
-      event.preventDefault();
-    }
-  }
-
   @HostListener('keyup.escape')
   cancel() {
     this.control.cancel();
@@ -140,26 +97,5 @@ export class TaskAddComponent implements OnInit, AfterViewInit, OnDestroy {
   private setSubflowWindowState(state: boolean) {
     this.isSubflowOpen = state;
     this.updateWindowState();
-  }
-
-  private ensureCurrentListOptionVisible() {
-    if (!this.keyboardEventsManager.activeItem) {
-      return;
-    }
-
-    const list = this.activityListRef.nativeElement;
-    const listBottom = list.scrollTop + list.clientHeight;
-
-    const currentListItem = this.optionListItemsRefs.toArray()[
-      this.keyboardEventsManager.activeItemIndex
-    ].nativeElement;
-    const currentElementTop = currentListItem.offsetTop - list.offsetTop;
-    const currentElementBottom = currentElementTop + currentListItem.clientHeight;
-
-    const isNotVisible =
-      currentElementTop < list.scrollTop || currentElementBottom > listBottom;
-    if (isNotVisible) {
-      currentListItem.scrollIntoView({ block: 'nearest', inline: 'nearest' });
-    }
   }
 }
--- libs/plugins/flow-client/src/lib/task-add/task-add.module.ts
+++ libs/plugins/flow-client/src/lib/task-add/task-add.module.ts
@@ -8,7 +8,8 @@ import { SubFlowModule } from '../sub-flow';
 import { AddActivityDirective } from './add-activity.directive';
 import { TaskAddComponent } from './task-add.component';
 import { AddActivityService } from './add-activity.service';
-import { ActivityComponent } from './activity/activity.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityComponent } from './activity-list/activity.component';
 
 @NgModule({
   imports: [
@@ -19,7 +20,12 @@ import { ActivityComponent } from './activity/activity.component';
     ContribInstallerModule,
     SubFlowModule,
   ],
-  declarations: [AddActivityDirective, TaskAddComponent, ActivityComponent],
+  declarations: [
+    AddActivityDirective,
+    TaskAddComponent,
+    ActivityComponent,
+    ActivityListComponent,
+  ],
   providers: [AddActivityService],
   entryComponents: [TaskAddComponent],
   exports: [AddActivityDirective],
